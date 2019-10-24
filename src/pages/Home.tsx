ae3e3480f85a893c12aa19846c47cfc8ac0410dc
Terhi Ollila <terhi.ollila@digia.com>
2019-10-24 13:22:57 +0300

    Fixed build errors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,11 @@ import PageLayout from '../common/layout/PageLayout';
 type Props = RouteChildrenProps & {};
 
 function Home(props: Props) {
-  return (<PageLayout>Home</PageLayout>);
+  return (
+    <PageLayout>
+      Home
+    </PageLayout>
+  );
 }
 
 export default Home;
