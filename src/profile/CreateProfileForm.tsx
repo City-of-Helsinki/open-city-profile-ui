c3743e01008ce6ed3db66d5c57d0db5eabb74dad
Jimi <jiripim@gmail.com>
2019-11-19 16:46:43 +0200

    Form validation

--- src/profile/CreateProfileForm.tsx
+++ src/profile/CreateProfileForm.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { TextInput } from 'hds-react';
 import { Formik, Form, Field } from 'formik';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  firstName: yup.string(),
+  lastName: yup.string(),
+  email: yup.string().email('validation.email'),
+  terms: yup.boolean().oneOf([true], 'validation.required'),
+});
 
 type Props = {
   profile: {
@@ -21,6 +29,9 @@ function CreateProfileForm(props: Props) {
         ...props.profile,
         terms: false,
       }}
+      initialErrors={{
+        terms: 'validation.required',
+      }}
       onSubmit={values => {
         props.onReady({
           firstName: values.firstName,
@@ -29,38 +40,49 @@ function CreateProfileForm(props: Props) {
           phone: values.lastName,
         });
       }}
+      validationSchema={schema}
     >
-      {props => (
+      {({ errors }) => (
         <Form>
           <Field
             name="firstName"
             id="firstName"
             as={TextInput}
+            invalid={errors.firstName}
+            invalidText={errors.firstName && t(errors.firstName)}
             labelText={t('profileForm.firstName')}
           />
           <Field
             name="lastName"
             id="lastName"
             as={TextInput}
+            invalid={errors.lastName}
+            invalidText={errors.lastName && t(errors.lastName)}
             labelText={t('profileForm.lastName')}
           />
           <Field
             name="email"
             id="email"
             as={TextInput}
+            invalid={errors.email}
+            invalidText={errors.email && t(errors.email)}
             labelText={t('profileForm.email')}
           />
           <Field
             name="phone"
             id="phone"
             as={TextInput}
+            invalid={errors.phone}
+            invalidText={errors.phone && t(errors.phone)}
             labelText={t('profileForm.phone')}
           />
           <label>
             <Field name="terms" type="checkbox" /> {t('profileForm.terms')}
           </label>
           <div>
-            <button type="submit">{t('profileForm.submit')}</button>
+            <button type="submit" disabled={Object.keys(errors).length > 0}>
+              {t('profileForm.submit')}
+            </button>
           </div>
         </Form>
       )}
