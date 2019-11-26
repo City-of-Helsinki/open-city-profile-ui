75a32d34154af97d8fa7a55693ed984e202aecdf
Jimi <jiripim@gmail.com>
2019-11-26 16:54:17 +0200

    Add footer to bottom of page

--- src/common/pageLayout/PageLayout.tsx
+++ src/common/pageLayout/PageLayout.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import Header from '../header/Header';
+import Footer from '../footer/Footer';
+import styles from './PageLayout.module.css';
 
 type Props = React.PropsWithChildren<{}>;
 
 function PageLayout(props: Props) {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
-      {props.children}
-    </>
+      <main className={styles.content}>{props.children}</main>
+      <Footer />
+    </div>
   );
 }
 
