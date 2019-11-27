9c9d31b1db3304ba7532ff9b291cd88f4ab69873
Jimi <jiripim@gmail.com>
2019-11-27 09:54:29 +0200

    Functionality for fetching profile
    Just fetching some fields to test that it works, add more fields when actually needed

--- src/profile/components/viewProfile/ViewProfile.tsx
+++ src/profile/components/viewProfile/ViewProfile.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { loader } from 'graphql.macro';
+
+const PROFILE = loader('../../graphql/profile.graphql');
 
 type Props = {};
 
 function Profile(props: Props) {
-  return <div>Profile view</div>;
+  const { data, loading, error } = useQuery(PROFILE);
+  return <div>{data && JSON.stringify(data)}</div>;
 }
 
 export default Profile;
