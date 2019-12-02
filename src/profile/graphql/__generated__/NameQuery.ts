c63b227578cdad20367a35ea3944bc4078449a8d
Jimi <jiripim@gmail.com>
2019-12-02 15:37:04 +0200

    Query for fetching name/nickname

---
+++ src/profile/graphql/__generated__/NameQuery.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: NameQuery
+// ====================================================
+
+export interface NameQuery_myProfile {
+  readonly __typename: "ProfileNode";
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly nickname: string | null;
+}
+
+export interface NameQuery {
+  readonly myProfile: NameQuery_myProfile | null;
+}
