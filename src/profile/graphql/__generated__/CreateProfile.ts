1389e04ac1269e19b2c56bdfc9fd1af1f083cc83
Jimi <jiripim@gmail.com>
2019-12-04 11:52:48 +0200

    Generate new types from graphql

--- src/profile/graphql/__generated__/CreateProfile.ts
+++ src/profile/graphql/__generated__/CreateProfile.ts
@@ -8,7 +8,7 @@ import { ProfileInput } from "./../../../graphql/__generated__/globalTypes";
 // GraphQL mutation operation: CreateProfile
 // ====================================================
 
-export interface CreateProfile_updateProfile_profile {
+export interface CreateProfile_createProfile_profile {
   readonly __typename: "ProfileNode";
   /**
    * The ID of the object.
@@ -16,13 +16,13 @@ export interface CreateProfile_updateProfile_profile {
   readonly id: string;
 }
 
-export interface CreateProfile_updateProfile {
-  readonly __typename: "UpdateProfile";
-  readonly profile: CreateProfile_updateProfile_profile | null;
+export interface CreateProfile_createProfile {
+  readonly __typename: "CreateProfile";
+  readonly profile: CreateProfile_createProfile_profile | null;
 }
 
 export interface CreateProfile {
-  readonly updateProfile: CreateProfile_updateProfile | null;
+  readonly createProfile: CreateProfile_createProfile | null;
 }
 
 export interface CreateProfileVariables {
