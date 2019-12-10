4476c0c85f5ae0736b7fec3d42e6fcc4a7bb0dee
Jimi <jiripim@gmail.com>
2019-12-10 13:03:40 +0200

    Add LanguageSwitcher to header

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import { ReactComponent as HelsinkiLogo } from '../svg/HelsinkiLogo.svg';
 import styles from './Header.module.css';
 import responsive from '../cssHelpers/responsive.module.css';
+import LanguageSwitcher from '../../i18n/components/languageSwitcher/LanguageSwitcher';
 
 type Props = {};
 
@@ -15,6 +16,9 @@ function Header(props: Props) {
       <div className={classNames(styles.inner, responsive.maxWidthCentered)}>
         <HelsinkiLogo className={styles.logo} aria-label="Helsinki logo" />
         <span className={`${styles.appName}`}>{t('appName')}</span>
+        <section className={styles.end}>
+          <LanguageSwitcher />
+        </section>
       </div>
     </header>
   );
