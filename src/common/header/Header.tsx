6d74bdad1a54d9b6f347076213ae7b3248224cd5
Jimi <jiripim@gmail.com>
2019-12-10 15:53:00 +0200

    Mobile navigation overlay

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import classNames from 'classnames';
 
 import { ReactComponent as HelsinkiLogo } from '../svg/HelsinkiLogo.svg';
+import { ReactComponent as HamburgerMenu } from '../svg/HamburgerMenu.svg';
+import { ReactComponent as Close } from '../svg/Close.svg';
 import styles from './Header.module.css';
 import responsive from '../cssHelpers/responsive.module.css';
 import LanguageSwitcher from '../../i18n/components/languageSwitcher/LanguageSwitcher';
@@ -11,13 +13,31 @@ type Props = {};
 
 function Header(props: Props) {
   const { t } = useTranslation();
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <header className={styles.header}>
       <div className={classNames(styles.inner, responsive.maxWidthCentered)}>
         <HelsinkiLogo className={styles.logo} aria-label="Helsinki logo" />
         <span className={`${styles.appName}`}>{t('appName')}</span>
-        <section className={styles.end}>
-          <LanguageSwitcher />
+        <section className={styles.navigation}>
+          {isNavOpen ? (
+            <Close
+              className={styles.navigationControl}
+              onClick={() => setIsNavOpen(false)}
+            />
+          ) : (
+            <HamburgerMenu
+              className={styles.navigationControl}
+              onClick={() => setIsNavOpen(true)}
+            />
+          )}
+          <div
+            className={classNames(styles.navigationItems, {
+              [styles.navigationItemsOpen]: isNavOpen,
+            })}
+          >
+            <LanguageSwitcher onLanguageChanged={() => setIsNavOpen(false)} />
+          </div>
         </section>
       </div>
     </header>
