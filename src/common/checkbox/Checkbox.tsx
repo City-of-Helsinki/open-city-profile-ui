35dc658db1083a1e7ffbde59871617130c195f61
Santtu <santtu.tuovinen2@gmail.com>
2020-01-03 13:43:52 +0200

    Common checkbox component

---
+++ src/common/checkbox/Checkbox.tsx
@@ -0,0 +1,33 @@
+import React, { PropsWithChildren } from 'react';
+import { Trans } from 'react-i18next';
+import classNames from 'classnames';
+import styles from './Checkbox.module.css';
+
+type Props = PropsWithChildren<{
+  onChange: () => void;
+  label?: string;
+  useTransComponent?: boolean;
+  components?: Array<any>;
+  i18Key?: string;
+  className?: string;
+}>;
+
+function Checkbox(props: Props) {
+  return (
+    <div className={classNames(styles.container, props.className)}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        onChange={props.onChange}
+      />
+      <span className={styles.label}>
+        {!props.useTransComponent && props.label}
+        {props.useTransComponent && (
+          <Trans i18nKey={props.i18Key} components={props.components} />
+        )}
+      </span>
+    </div>
+  );
+}
+
+export default Checkbox;
