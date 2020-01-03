ca29fd9ec8a29029844d75915a67bbbec33daa8e
Jimi <jiripim@gmail.com>
2020-01-03 17:33:37 +0200

    List the connected services

--- src/profile/components/serviceConnections/ServiceConnections.tsx
+++ src/profile/components/serviceConnections/ServiceConnections.tsx
@@ -5,24 +5,33 @@ import { useQuery } from '@apollo/react-hooks';
 
 import responsive from '../../../common/cssHelpers/responsive.module.css';
 import Explanation from '../../../common/explanation/Explanation';
+import ExpandingPanel from '../../../common/expandingPanel/ExpandingPanel';
 import styles from './ServiceConnections.module.css';
 import { ServiceConnectionQuery } from '../../graphql/__generated__/ServiceConnectionQuery';
+import { ServiceType } from '../../../graphql/__generated__/globalTypes';
 
 const SERVICE_CONNECTIONS = loader(
   '../../graphql/ServiceConnectionsQuery.graphql'
 );
 
+const getServiceTypes = (data?: ServiceConnectionQuery) => {
+  if (data?.myProfile?.serviceConnections) {
+    return data.myProfile.serviceConnections.edges.map(edge => {
+      if (edge?.node?.service.type) {
+        return edge.node.service.type;
+      }
+      return false;
+    });
+  }
+  return [];
+};
+
 type Props = {};
 
 function ServiceConnections(props: Props) {
   const { t } = useTranslation();
-  const { data, loading, error } = useQuery<ServiceConnectionQuery>(
-    SERVICE_CONNECTIONS
-  );
-  const serviceTypes = data?.myProfile?.serviceConnections?.edges.map(
-    edge => edge?.node?.service.type
-  );
-  console.log(serviceTypes);
+  const { data } = useQuery<ServiceConnectionQuery>(SERVICE_CONNECTIONS);
+  const services = getServiceTypes(data);
   return (
     <div className={styles.serviceConnections}>
       <div className={responsive.maxWidthCentered}>
@@ -31,6 +40,12 @@ function ServiceConnections(props: Props) {
           main={t('serviceConnections.title')}
           small={t('serviceConnections.explanation')}
         />
+        {services.map(
+          (service, index) =>
+            service && (
+              <ExpandingPanel key={index} title={ServiceType[service]} />
+            )
+        )}
       </div>
     </div>
   );
