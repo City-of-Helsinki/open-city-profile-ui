9925782ce1437e2987c0e6c9cd7fc7c173ab40cc
Jimi <jiripim@gmail.com>
2020-01-09 11:35:21 +0200

    Make ExpandingPanel more accessible

--- src/common/expandingPanel/ExpandingPanel.tsx
+++ src/common/expandingPanel/ExpandingPanel.tsx
@@ -10,12 +10,28 @@ type Props = PropsWithChildren<{
 
 function ExpandingPanel(props: Props) {
   const [expanded, setExpanded] = useState(false);
-
   const toggleExpanding = () => setExpanded(prevState => !prevState);
+  const onKeyDown = (event: React.KeyboardEvent) => {
+    if (
+      event.key === ' ' ||
+      event.key === 'Enter' ||
+      event.key === 'Spacebar'
+    ) {
+      event.preventDefault();
+      toggleExpanding();
+    }
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.title} onClick={toggleExpanding}>
+      <div
+        className={styles.title}
+        onClick={toggleExpanding}
+        onKeyDown={onKeyDown}
+        tabIndex={0}
+        role="button"
+        aria-expanded={expanded ? 'true' : 'false'}
+      >
         <h2>{props.title}</h2>
         <IconAngleRight
           className={expanded ? styles.iconUp : styles.iconDown}
