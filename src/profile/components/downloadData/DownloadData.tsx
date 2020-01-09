b449a2348f27fbbd614385f5755048f095b12f5a
Jimi <jiripim@gmail.com>
2020-01-09 11:08:52 +0200

    Start implementing DownloadData component #wip

--- src/profile/components/downloadData/DownloadData.tsx
+++ src/profile/components/downloadData/DownloadData.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import Button from '../../../common/button/Button';
+import ExpandingPanel from '../../../common/expandingPanel/ExpandingPanel';
+import styles from './DownloadData.module.css';
 
 type Props = {};
 
 function DownloadData(props: Props) {
-  return null;
+  const downloadData = () => {};
+  const { t } = useTranslation();
+  return (
+    <ExpandingPanel title={t('downloadData.panelTitle')}>
+      <span>{t('downloadData.panelText')}</span>
+      <Button onClick={downloadData}>{t('downloadData.button')}</Button>
+    </ExpandingPanel>
+  );
 }
 
 export default DownloadData;
