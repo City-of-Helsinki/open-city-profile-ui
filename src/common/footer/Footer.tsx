64c0fbeca16382e78381f28cb967bd9369904ee7
Santtu <santtu.tuovinen2@gmail.com>
2020-01-15 13:36:04 +0200

    Added links and link-styles.

--- src/common/footer/Footer.tsx
+++ src/common/footer/Footer.tsx
@@ -4,13 +4,17 @@ import { ReactComponent as HelsinkiLogo } from '../svg/HelsinkiLogo.svg';
 import Copyright from '../copyright/Copyright';
 import styles from './Footer.module.css';
 import responsive from '../cssHelpers/responsive.module.css';
+import FooterLinks from '../footerLinks/FooterLinks';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={responsive.maxWidthCentered}>
         <HelsinkiLogo className={styles.logo} aria-label="Helsinki logo" />
-        <Copyright className={styles.copyright} />
+        <div className={styles.textContainer}>
+          <Copyright />
+          <FooterLinks className={styles.links} />
+        </div>
       </div>
     </footer>
   );
---
+++ src/common/footerLinks/FooterLinks.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  className?: string;
+};
+
+function FooterLinks(props: Props) {
+  return (
+    <span className={props.className}>
+      {' '}
+      <Link to="/#">Rekisteriseloste</Link> |{' '}
+      <Link to="/#">Saavutettavuusseloste</Link> |{' '}
+      <Link to="/">Käyttöehdot</Link>
+    </span>
+  );
+}
+
+export default FooterLinks;
