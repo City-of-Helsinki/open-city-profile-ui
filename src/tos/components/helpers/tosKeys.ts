bc1e22168e13445da5f9335ff2ea1318f082447d
Santtu <santtu.tuovinen2@gmail.com>
2020-01-16 09:52:36 +0200

    Fixed with prettier

--- src/tos/components/helpers/tosKeys.ts
+++ src/tos/components/helpers/tosKeys.ts
@@ -1,12 +1,12 @@
 export default [
-  {title: 'tos.title1', message: 'tos.message1'},
-  {title: 'tos.title2', message: 'tos.message2', links : ['']},
-  {title: 'tos.title3', message: 'tos.message3'},
-  {title: 'tos.title4', message: 'tos.message4'},
-  {title: 'tos.title5', message: 'tos.message5'},
-  {title: 'tos.title6', message: 'tos.message6', links: ['', '']},
-  {title: 'tos.title7', message: 'tos.message7'},
-  {title: 'tos.title8', message: 'tos.message8'},
-  {title: 'tos.title9', message: 'tos.message9'},
-  {title: 'tos.title10', message: 'tos.message10'},
-]
+  { title: 'tos.title1', message: 'tos.message1' },
+  { title: 'tos.title2', message: 'tos.message2', links: [''] },
+  { title: 'tos.title3', message: 'tos.message3' },
+  { title: 'tos.title4', message: 'tos.message4' },
+  { title: 'tos.title5', message: 'tos.message5' },
+  { title: 'tos.title6', message: 'tos.message6', links: ['', ''] },
+  { title: 'tos.title7', message: 'tos.message7' },
+  { title: 'tos.title8', message: 'tos.message8' },
+  { title: 'tos.title9', message: 'tos.message9' },
+  { title: 'tos.title10', message: 'tos.message10' },
+];
