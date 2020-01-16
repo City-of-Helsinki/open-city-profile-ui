1f4ddae1497d5c9ec5831262301fba1f962e45e4
Santtu <santtu.tuovinen2@gmail.com>
2020-01-16 12:02:40 +0200

    Translations for footer.

--- src/common/copyright/Copyright.tsx
+++ src/common/copyright/Copyright.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   className?: string;
 };
 
 function Copyright(props: Props) {
+  const { t } = useTranslation();
   const year = new Date().getFullYear();
+
   return (
     <span className={props.className}>
-      &copy; Copyright {year} &middot; Helsingin kaupunki &middot; All rights
-      reserved &middot;
+      &copy; {t('footer.copyright', { year })} &middot; Helsingin kaupunki
+      &middot; {t('footer.reserveRights')} &middot;
     </span>
   );
 }
--- src/common/footerLinks/FooterLinks.tsx
+++ src/common/footerLinks/FooterLinks.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+
+import tosConstants from '../../tos/constants/tosConstants';
 
 type Props = {
   className?: string;
 };
 
 function FooterLinks(props: Props) {
+  const { t, i18n } = useTranslation();
+  const selectedLanguage =
+    (i18n.languages && i18n.languages[0].toUpperCase()) || 'FI';
+  const link = Object(tosConstants.REGISTER_DESCRIPTION)[selectedLanguage];
   return (
     <span className={props.className}>
       {' '}
-      <Link to="/#">Rekisteriseloste</Link> |{' '}
-      <Link to="/#">Saavutettavuusseloste</Link> |{' '}
-      <Link to="/terms-of-service">Käyttöehdot</Link>
+      <a href={link}>{t('footer.privacy')}</a> |{' '}
+      <Link to="/#">{t('footer.accessibility')}</Link> |{' '}
+      <Link to="/terms-of-service">{t('footer.terms')}</Link>
     </span>
   );
 }
