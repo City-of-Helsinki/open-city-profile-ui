823652acf957bfac4d9cb0f8227e21c050cdc90b
Jimi <jiripim@gmail.com>
2020-01-27 15:06:09 +0200

    Refactor getServiceTypes helper to only return types of ServiceType

--- src/profile/helpers/getServiceTypes.ts
+++ src/profile/helpers/getServiceTypes.ts
@@ -1,13 +1,20 @@
-import { ServiceConnectionsQuery } from '../../graphql/generatedTypes';
+import {
+  ServiceConnectionsQuery,
+  ServiceType,
+} from '../../graphql/generatedTypes';
 
 export default function getServiceTypes(data?: ServiceConnectionsQuery) {
   if (data?.myProfile?.serviceConnections) {
-    return data.myProfile.serviceConnections.edges.map(edge => {
-      if (edge?.node?.service.type) {
-        return edge.node.service.type;
-      }
-      return false;
-    });
+    return data.myProfile.serviceConnections.edges
+      .map(edge => {
+        if (edge?.node?.service.type) {
+          return edge.node.service.type;
+        }
+        return false;
+      })
+      .filter((serviceType): serviceType is ServiceType =>
+        Boolean(serviceType)
+      );
   }
   return [];
 }
