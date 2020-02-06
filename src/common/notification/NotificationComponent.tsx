b263a2a1e0509502d1c22137dfc7cb8ba8798d98
Santtu <santtu.tuovinen2@gmail.com>
2020-02-06 09:28:13 +0200

    Component for showing errors on backend calls.

---
+++ src/common/notification/NotificationComponent.tsx
@@ -0,0 +1,31 @@
+import React, { PropsWithChildren } from 'react';
+import { DismissableNotification } from 'hds-react';
+import { useTranslation } from 'react-i18next';
+
+import styles from './NotificationComponent.module.css';
+
+type Props = PropsWithChildren<{
+  show: boolean;
+  labelText?: string;
+  type?: 'error' | 'success' | 'warning' | 'notification';
+  onClose?: () => void;
+}>;
+
+function NotificationComponent(props: Props) {
+  const { t } = useTranslation();
+  if (!props.show) return null;
+  return (
+    <div className={styles.notification}>
+      <DismissableNotification
+        type={props.type || 'error'}
+        labelText={props.labelText || t('notification.defaultErrorTitle')}
+        closeButtonLabelText={t('notification.closeButtonText')}
+        onClose={props.onClose}
+      >
+        {props.children || t('notification.defaultErrorText')}
+      </DismissableNotification>
+    </div>
+  );
+}
+
+export default NotificationComponent;
