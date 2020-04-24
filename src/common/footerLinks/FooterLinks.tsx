ee8a7181cb68a120bbf621d19eb521194f7ab984
Santtu Tuovinen <santtu.tuovinen2@gmail.com>
2020-04-24 11:51:48 +0300

    Removed all the unused components.

--- src/common/footerLinks/FooterLinks.tsx
+++ src/common/footerLinks/FooterLinks.tsx
@@ -2,21 +2,17 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-import tosConstants from '../../tos/constants/tosConstants';
-
 type Props = {
   className?: string;
 };
 
 function FooterLinks(props: Props) {
-  const { t, i18n } = useTranslation();
-  const selectedLanguage =
-    (i18n.languages && i18n.languages[0].toUpperCase()) || 'FI';
-  const link = Object(tosConstants.REGISTER_DESCRIPTION)[selectedLanguage];
+  const { t } = useTranslation();
+
   return (
     <span className={props.className}>
       {' '}
-      <a href={link}>{t('footer.privacy')}</a> |{' '}
+      <a href="/#">{t('footer.privacy')}</a> |{' '}
       <Link to="/#">{t('footer.accessibility')}</Link>
     </span>
   );
