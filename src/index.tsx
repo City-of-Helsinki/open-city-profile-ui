239ef2ec6b743f495867be6a35090b21bc95b770
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-05-25 08:40:58 +0300

    Fix sentry environment always pointing to production.

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,12 @@ Modal.setAppElement('#root');
 
 const ENVS_WITH_SENTRY = ['staging', 'production'];
 
-if (ENVS_WITH_SENTRY.includes(process.env.NODE_ENV)) {
+if (
+  ENVS_WITH_SENTRY.includes(process.env.REACT_APP_ENVIRONMENT || 'development')
+) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
-    environment: process.env.NODE_ENV,
+    environment: process.env.REACT_APP_ENVIRONMENT,
   });
 }
 
