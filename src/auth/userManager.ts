cd6c3e1c0ebf95f52a9c6d64352241bfe0f851c0
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-05-27 11:33:37 +0300

    Fix silent renew

--- src/auth/userManager.ts
+++ src/auth/userManager.ts
@@ -5,9 +5,7 @@ import store from '../redux/store';
 import { fetchApiTokenThunk } from './redux';
 import getAuthenticatedUser from './getAuthenticatedUser';
 
-const location = `${window.location.protocol}//${window.location.hostname}${
-  window.location.port ? `:${window.location.port}` : ''
-}`;
+const location = window.location.origin;
 
 /* eslint-disable @typescript-eslint/camelcase */
 const settings: UserManagerSettings = {
@@ -18,8 +16,11 @@ const settings: UserManagerSettings = {
   redirect_uri: `${location}/callback`,
   response_type: 'id_token token',
   scope: process.env.REACT_APP_OIDC_SCOPE,
-  silent_redirect_uri: `${location}/silent_renew`,
+  silent_redirect_uri: `${location}/silent_renew.html`,
   post_logout_redirect_uri: `${location}/`,
+  // This calculates to 1 minute, good for debugging:
+  // https://github.com/City-of-Helsinki/kukkuu-ui/blob/8029ed64c3d0496fa87fa57837c73520e8cbe37f/src/domain/auth/userManager.ts#L18
+  // accessTokenExpiringNotificationTime: 59.65 * 60,
 };
 /* eslint-enable @typescript-eslint/camelcase */
 
