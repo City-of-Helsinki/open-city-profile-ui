118b978f6eb76d029edfe14e0f1e86d61b0ae751
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-06-02 05:59:35 +0300

    Updated tests for email deletion.

--- src/profile/helpers/__tests__/updateMutationVariables.test.ts
+++ src/profile/helpers/__tests__/updateMutationVariables.test.ts
@@ -1,5 +1,6 @@
 import {
   EmailType,
+  MyProfileQuery,
   MyProfileQuery_myProfile_emails_edges_node as Email,
 } from '../../../graphql/generatedTypes';
 import { getEmail } from '../updateMutationVariables';
@@ -21,18 +22,80 @@ const emails: Email[] = [
     id: '',
     __typename: 'EmailNode',
   },
+  {
+    email: 'kolmas@testi.fi',
+    primary: false,
+    emailType: EmailType.OTHER,
+    id: '234',
+    __typename: 'EmailNode',
+  },
 ];
+
+const getMyProfileQuery = (variables?: MyProfileQuery) => {
+  return {
+    myProfile: {
+      firstName: 'Teemu',
+      lastName: 'Testaaja',
+      primaryEmail: {
+        email: 'ensimmainen@testi.fi',
+        primary: true,
+        id: '123',
+        __typename: 'EmailNode',
+      },
+      emails: {
+        edges: [
+          {
+            node: {
+              email: 'toinen@testi.fi',
+              primary: false,
+              emailType: EmailType.OTHER,
+              id: '',
+              __typename: 'EmailNode',
+            },
+            __typename: 'EmailNodeEdge',
+          },
+          {
+            node: {
+              email: 'kolmas@testi.fi',
+              primary: false,
+              emailType: EmailType.OTHER,
+              id: '234',
+              __typename: 'EmailNode',
+            },
+            __typename: 'EmailNodeEdge',
+          },
+        ],
+        __typename: 'EmailNodeConnection',
+      },
+
+      __typename: 'ProfileNode',
+      ...variables?.myProfile,
+    },
+  } as MyProfileQuery;
+};
 
 describe('test getEmails function', () => {
   test('add and update arrays are formed correctly', () => {
     const emailObj = getEmail(emails);
 
     expect(emailObj.addEmails.length).toEqual(1);
-    expect(emailObj.updateEmails.length).toEqual(1);
+    expect(emailObj.updateEmails.length).toEqual(2);
   });
 
   test('add array is null', () => {
     const emailObj = getEmail([{ ...emails[0] }]);
     expect(emailObj.addEmails).toEqual([null]);
+  });
+
+  test('removeEmails field doesnt exist', () => {
+    const myProfile = getMyProfileQuery();
+    const emailObj = getEmail(emails, myProfile);
+    expect(emailObj.removeEmails).toBeFalsy();
+  });
+
+  test('removeEmails exists', () => {
+    const myProfile = getMyProfileQuery();
+    const emailObj = getEmail([{ ...emails[0] }], myProfile);
+    expect(emailObj.removeEmails).toEqual(['234']);
   });
 });
