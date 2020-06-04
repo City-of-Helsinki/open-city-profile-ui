143bbbbaefd5c98ee3bbe1c73f7e515705ec1d50
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-06-04 11:16:33 +0300

    Fix test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from './App';
+import { MemoryRouter } from 'react-router';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
 });
