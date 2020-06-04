cafb44d8b45a8817cfcd349a51dd3a2bc417daa7
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-06-04 11:21:02 +0300

    Fix import order

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
 import App from './App';
-import { MemoryRouter } from 'react-router';
 
 it('renders without crashing', () => {
   shallow(
