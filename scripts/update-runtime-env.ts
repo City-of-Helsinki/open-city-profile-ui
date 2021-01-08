5113f18ab67956ab8c26c18164b9546c0be32093
Juho Saarinen <juho.saarinen@gmail.com>
2021-01-08 09:38:50 +0200

    Documentation updated

--- scripts/update-runtime-env.ts
+++ scripts/update-runtime-env.ts
@@ -1,26 +1,25 @@
 #!/usr/bin/env ts-node-script
 
-// /// <reference types="./helsinki-utils" />
-// import fetchTranslations from 'helsinki-utils/scripts/fetch-translations';
-
-// const languages = process.env.TRANSLATION_LANGUAGES.split(',');
-// const sheetId = process.env.TRANSLATIONS_SHEET_ID;
-
-// const pathToLocales: string = path.join(__dirname, '../src/i18n');
-
 import * as path from 'path';
 import fs from 'fs';
 import util from 'util';
+
+// react-scipts config requires ENV to be set
+const defaultNodeEnv = process.env.TEST ? 'test' : 'development';
+// Prevent collision is app is running while tests are started
+const configFile = process.env.TEST ? 'test-env-config.js' : 'env-config.js';
+
+process.env.NODE_ENV=process.env.NODE_ENV || defaultNodeEnv;
+
 const getClientEnvironment = require('../node_modules/react-scripts/config/env.js');
 
 
-const neededEnvVariablePrefixes: string[] = ["REACT_APP", "TRANSLATION"]
-const condigurationFile: string = path.join(__dirname, '../public/env-config.js');
+const configurationFile: string = path.join(__dirname, '../public/'+configFile);
 
 const start = async () => {
   try {
-    const envVariables = getClientEnvironment()
-    fs.writeFile(condigurationFile, 'window._env_ = ' + util.inspect(envVariables.raw, false, 2, false), function (err) {
+    const envVariables = getClientEnvironment();
+    fs.writeFile(configurationFile, 'window._env_ = ' + util.inspect(envVariables.raw, false, 2, false), function (err) {
       if (err) {
         return console.error(err);
       }
