d9dd9e30470682f6cefc4448f7fbe14592c35bb0
NikoHelle <niko.helle@hyperactive.fi>
2021-03-16 16:49:24 +0200

    Show suomi.fi as an AMR.
    Also DownloadData and DeleteProfile components inside ProfileInformation cause unnecessary graphQL reloads when Profile changes. Moved their code to independent components to avoid reloading

--- src/profile/components/deleteProfile/DeleteProfile.tsx
+++ src/profile/components/deleteProfile/DeleteProfile.tsx
@@ -4,34 +4,50 @@ import { loader } from 'graphql.macro';
 import { useTranslation } from 'react-i18next';
 import * as Sentry from '@sentry/browser';
 import { Checkbox } from 'hds-react';
+import { useHistory } from 'react-router';
+import { useMatomo } from '@datapunt/matomo-tracker-react';
 
 import ConfirmationModal from '../modals/confirmationModal/ConfirmationModal';
 import ExpandingPanel from '../../../common/expandingPanel/ExpandingPanel';
 import Button from '../../../common/button/Button';
-import { ServiceConnectionsQuery } from '../../../graphql/generatedTypes';
+import { ServiceConnectionsQuery as ServiceConnectionsRoot } from '../../../graphql/generatedTypes';
 import useToast from '../../../toast/useToast';
 import styles from './deleteProfile.module.css';
+import useDeleteProfile from '../../../gdprApi/useDeleteProfile';
+import checkBerthError from '../../helpers/checkBerthError';
+import BerthErrorModal from '../modals/berthError/BerthErrorModal';
 
 const SERVICE_CONNECTIONS = loader(
   '../../graphql/ServiceConnectionsQuery.graphql'
 );
 
-type Props = {
-  isOpenByDefault: boolean;
-  onDelete: () => void;
-};
-
-function DeleteProfile({
-  isOpenByDefault,
-  onDelete,
-}: Props): React.ReactElement {
+function DeleteProfile(): React.ReactElement {
   const [deleteConfirmationModal, setDeleteConfirmationModal] = useState(false);
   const [deleteInstructions, setDeleteInstructions] = useState(false);
   const { createToast } = useToast();
+  const history = useHistory();
+  const { trackEvent } = useMatomo();
+  const [berthError, setBerthError] = useState(false);
+  const [deleteProfile, deleteProfileResult] = useDeleteProfile({
+    onCompleted: returnedData => {
+      if (returnedData) {
+        trackEvent({ category: 'action', action: 'Delete profile' });
+        history.push('/profile-deleted');
+      }
+    },
+    onError: error => {
+      if (checkBerthError(error.graphQLErrors)) {
+        setBerthError(true);
+      } else {
+        Sentry.captureException(error);
+        createToast({ type: 'error' });
+      }
+    },
+  });
 
   const { t, i18n } = useTranslation();
 
-  const { data, refetch } = useQuery<ServiceConnectionsQuery>(
+  const { data, refetch } = useQuery<ServiceConnectionsRoot>(
     SERVICE_CONNECTIONS,
     {
       onError: (error: Error) => {
@@ -64,10 +80,11 @@ function DeleteProfile({
       throw Error('Could not find services to delete');
     }
 
-    onDelete();
+    deleteProfile();
   };
   const userHasServices =
     data?.myProfile?.serviceConnections?.edges?.length !== 0;
+  const isOpenByDefault = deleteProfileResult.loading;
 
   return (
     <React.Fragment>
@@ -108,6 +125,10 @@ function DeleteProfile({
             : t('deleteProfileModal.noServiceExplanation')
         }
         actionButtonText={t('deleteProfileModal.delete')}
+      />
+      <BerthErrorModal
+        isOpen={berthError}
+        onClose={() => setBerthError(prevState => !prevState)}
       />
     </React.Fragment>
   );
--- src/profile/components/profileInformation/ProfileInformation.tsx
+++ src/profile/components/profileInformation/ProfileInformation.tsx
@@ -2,11 +2,6 @@ import React, { Fragment, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, IconPenLine } from 'hds-react';
 import countries from 'i18n-iso-countries';
-import { loader } from 'graphql.macro';
-import * as Sentry from '@sentry/browser';
-import FileSaver from 'file-saver';
-import { useHistory } from 'react-router';
-import { useMatomo } from '@datapunt/matomo-tracker-react';
 
 import DeleteProfile from '../deleteProfile/DeleteProfile';
 import LabeledValue from '../../../common/labeledValue/LabeledValue';
@@ -16,7 +11,6 @@ import getAddress from '../../helpers/getAddress';
 import getLanguageCode from '../../../common/helpers/getLanguageCode';
 import getName from '../../helpers/getName';
 import {
-  DownloadMyProfileQuery,
   MyProfileQuery,
   MyProfileQuery_myProfile_addresses_edges_node as Address,
   MyProfileQuery_myProfile_emails_edges_node as Email,
@@ -26,15 +20,9 @@ import ProfileSection from '../../../common/profileSection/ProfileSection';
 import getEmailsFromNode from '../../helpers/getEmailsFromNode';
 import getAddressesFromNode from '../../helpers/getAddressesFromNode';
 import getPhonesFromNode from '../../helpers/getPhonesFromNode';
-import useDownloadProfile from '../../../gdprApi/useDownloadProfile';
-import useDeleteProfile from '../../../gdprApi/useDeleteProfile';
-import checkBerthError from '../../helpers/checkBerthError';
 import BerthErrorModal from '../modals/berthError/BerthErrorModal';
 import ProfileInformationAccountManagementLink from './ProfileInformationAccountManagementLink';
-import useToast from '../../../toast/useToast';
 import VerifiedPersonalInformation from '../verifiedPersonalInformation/VerifiedPersonalInformation';
-
-const ALL_DATA = loader('../../graphql/DownloadMyProfileQuery.graphql');
 
 type Props = {
   loading: boolean;
@@ -44,49 +32,8 @@ type Props = {
 };
 
 function ProfileInformation(props: Props): React.ReactElement {
-  const history = useHistory();
   const { t, i18n } = useTranslation();
-  const { trackEvent } = useMatomo();
   const [berthError, setBerthError] = useState(false);
-  const { createToast } = useToast();
-
-  // useDownloadProfile and useDeleteProfile need to be mounted when
-  // the page they are on is first rendered. That's why it's sensible to
-  // manage them in a component that makes the root of a route.
-  const [downloadProfileData, downloadQueryResult] = useDownloadProfile<
-    DownloadMyProfileQuery
-  >(ALL_DATA, {
-    onCompleted: returnedData => {
-      const blob = new Blob([returnedData.downloadMyProfile], {
-        type: 'application/json',
-      });
-      FileSaver.saveAs(blob, 'helsinkiprofile_data.json');
-    },
-    onError: (error: Error) => {
-      Sentry.captureException(error);
-      createToast({ type: 'error' });
-    },
-    fetchPolicy: 'network-only',
-  });
-  const [deleteProfile, deleteProfileResult] = useDeleteProfile({
-    onCompleted: returnedData => {
-      if (returnedData) {
-        trackEvent({ category: 'action', action: 'Delete profile' });
-        history.push('/profile-deleted');
-      }
-    },
-    onError: error => {
-      if (checkBerthError(error.graphQLErrors)) {
-        setBerthError(true);
-      } else {
-        Sentry.captureException(error);
-        createToast({ type: 'error' });
-      }
-    },
-  });
-
-  const isDownloadingProfile = downloadQueryResult.loading;
-  const isDeletingProfile = deleteProfileResult.loading;
 
   const { isEditing, setEditing, loading, data } = props;
 
@@ -194,15 +141,8 @@ function ProfileInformation(props: Props): React.ReactElement {
       </ProfileSection>
       <div className={styles.boxGrid}>
         <ProfileInformationAccountManagementLink />
-        <DownloadData
-          isDownloadingData={isDownloadingProfile}
-          isOpenByDefault={isDownloadingProfile}
-          onDownloadClick={downloadProfileData}
-        />
-        <DeleteProfile
-          onDelete={deleteProfile}
-          isOpenByDefault={isDeletingProfile}
-        />
+        <DownloadData />
+        <DeleteProfile />
       </div>
       <BerthErrorModal
         isOpen={berthError}
