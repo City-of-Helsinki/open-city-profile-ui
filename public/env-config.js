204faa3ef0b12ab9193dc0df70f643164c345762
NikoHelle <niko.helle@hyperactive.fi>
2021-03-19 14:57:29 +0200

    Updated development configs to use OpenShift servers

--- public/env-config.js
+++ public/env-config.js
@@ -1,12 +1,16 @@
 window._env_ = {
   NODE_ENV: 'development',
   PUBLIC_URL: undefined,
-  REACT_APP_OIDC_AUTHORITY: 'https://api.hel.fi/sso-test/',
-  REACT_APP_OIDC_CLIENT_ID: 'https://api.hel.fi/auth/helsinkiprofile-ui',
-  REACT_APP_PROFILE_GRAPHQL: 'https://profiili-api.test.kuva.hel.ninja/graphql/',
+  WDS_SOCKET_HOST: undefined,
+  WDS_SOCKET_PATH: undefined,
+  WDS_SOCKET_PORT: undefined,
+  FAST_REFRESH: true,
+  REACT_APP_OIDC_AUTHORITY: 'https://tunnistamo.dev.hel.ninja/',
+  REACT_APP_OIDC_CLIENT_ID: 'https://api.hel.fi/auth/helsinkiprofile-ui-dev',
+  REACT_APP_PROFILE_AUDIENCE: 'https://api.hel.fi/auth/helsinkiprofiledev',
+  REACT_APP_PROFILE_GRAPHQL: 'https://helsinki-profile-api-dev.agw.arodevtest.hel.fi/graphql/',
+  REACT_APP_OIDC_SCOPE: 'openid profile https://api.hel.fi/auth/helsinkiprofiledev',
   REACT_APP_APPLICATION_NAME: 'open-city-profile-ui',
-  REACT_APP_PROFILE_AUDIENCE: 'https://api.hel.fi/auth/helsinkiprofile',
-  REACT_APP_OIDC_SCOPE: 'openid profile https://api.hel.fi/auth/helsinkiprofile',
   REACT_APP_SENTRY_DSN: 'https://8b5b23e2171b42cd8617e2b1ad7353b6@sentry.hel.ninja/63',
-  REACT_APP_VERSION: '1.1.0'
+  REACT_APP_VERSION: '1.1.3'
 }
