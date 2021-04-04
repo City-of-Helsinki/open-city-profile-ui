361f1b826caae5fc9941f060c5b56702a3a0699b
NikoHelle <niko.helle@hyperactive.fi>
2021-04-04 12:27:03 +0300

    exposeProfileContext added to componentMocking
    React hooks are exposed to tests with @testing-library/react-hooks. It can return all props returned from the hook.
    
    useProfileContext required Apollo client so hook is wrapped with MockApolloClientProvider. And ProfileProvider to provide to hook to children.
    
    waitForUpdate and waitForDataChange are helper functions for tests to wait for updates from graphQL queries. testing-library has waitForNextUpdate, but it does not update everytime.

--- src/common/test/componentMocking.tsx
+++ src/common/test/componentMocking.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { render, RenderResult } from '@testing-library/react';
+import { renderHook, RenderHookResult } from '@testing-library/react-hooks';
+import _ from 'lodash';
+
+import {
+  ProfileContext,
+  ProfileContextData,
+  Provider as ProfileProvider,
+} from '../../profile/context/ProfileContext';
+import {
+  MockApolloClientProvider,
+  ResponseProvider,
+} from './MockApolloClientProvider';
 
 type ElementSelector = {
   testId?: string;
@@ -29,4 +41,76 @@ export const renderProfileContextWrapper = async (
     ...renderResult,
     getElement,
   });
+};
+
+export const exposeProfileContext = (
+  responseProvider: ResponseProvider
+): RenderHookResult<
+  {
+    children: React.ReactNodeArray;
+  },
+  ProfileContextData
+> & {
+  waitForDataChange: () => Promise<ProfileContextData>;
+  waitForUpdate: () => Promise<ProfileContextData>;
+} => {
+  const wrapper = ({ children }: { children: React.ReactNodeArray }) => (
+    <MockApolloClientProvider responseProvider={responseProvider}>
+      <ProfileProvider>{children}</ProfileProvider>
+    </MockApolloClientProvider>
+  );
+  const createUpdateProps = (
+    contextData: ProfileContextData
+  ): Record<string, unknown> => ({
+    loading: contextData.loading,
+    isComplete: contextData.isComplete,
+    error: contextData.error,
+  });
+  let lastUpdate: Record<string, unknown> = {};
+  let lastData: string | undefined;
+  let updateChangeResolver:
+    | ((newContextData: ProfileContextData) => void)
+    | undefined;
+  let dataChangeResolver:
+    | ((newContextData: ProfileContextData) => void)
+    | undefined;
+
+  //testing-library has waitForNextUpdate but it does not work properly!
+  const waitForUpdate = () =>
+    new Promise<ProfileContextData>(resolve => {
+      updateChangeResolver = resolve;
+    });
+  const waitForDataChange = () =>
+    new Promise<ProfileContextData>(resolve => {
+      dataChangeResolver = resolve;
+    });
+
+  const trackUpdateChanges = (newContext: ProfileContextData): void => {
+    const updateProps = createUpdateProps(newContext);
+    if (!_.isEqual(lastUpdate, updateProps)) {
+      lastUpdate = updateProps;
+      updateChangeResolver && updateChangeResolver(newContext);
+    }
+  };
+
+  const trackDataChanges = (newContext: ProfileContextData): void => {
+    const newData = JSON.stringify(newContext.data || {});
+    if (newData !== lastData) {
+      dataChangeResolver && dataChangeResolver(newContext);
+      dataChangeResolver = undefined;
+      lastData = newData;
+    }
+  };
+
+  const tracker = (newContextData: ProfileContextData) => {
+    trackDataChanges(newContextData);
+    trackUpdateChanges(newContextData);
+    return newContextData;
+  };
+  const callback = () =>
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    tracker(useContext(ProfileContext));
+
+  const result = renderHook(callback, { wrapper });
+  return { ...result, waitForDataChange, waitForUpdate };
 };
