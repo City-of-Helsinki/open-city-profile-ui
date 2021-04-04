c8c4bd4207eae87e26d71fc573639ae4ffad7947
NikoHelle <niko.helle@hyperactive.fi>
2021-04-04 18:14:30 +0300

    (HP-318) ProfileContext has getName and errorListeners
    As ProfileContext is the source for MyProfile it should also provide utility functions like getName()
    
    Added also errorListeners. addErrorListener returns a disposer function that removes that listener.
    
    useProfileErrorListener hook uses profileContext.addErrorListener but it also handles disposing when component is removed via useEffect hook. UseMemo is used so addListener is called only once. Using usePersistFn makes it safe to use changing functions as listeners and useMemo dependency. It stores latest version of the listener without invoking dependency change.
    
    Added error tracking to componentMocking and unique errors to MockApolloClientProvider

--- src/profile/context/__tests__/ProfileContext.test.tsx
+++ src/profile/context/__tests__/ProfileContext.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, waitFor } from '@testing-library/react';
 import { act, cleanup as cleanupHooks } from '@testing-library/react-hooks';
 
 import { getMyProfile } from '../../../common/test/myProfileMocking';
@@ -56,26 +56,44 @@ describe('ProfileContext', () => {
       expect(context.isInitialized).toEqual(true);
       expect(context.isComplete).toEqual(true);
       expect(context.data?.myProfile?.firstName).toEqual('Teemu');
+      expect(context.getName()).toEqual('Teemu Testaaja');
+      expect(context.getName(true)).toEqual('Teme');
     });
   });
-  it('Fetch errors are handled', async () => {
-    const responses: MockedResponse[] = [{ errorType: 'graphQLError' }];
-    const { result, waitForUpdate } = createTestEnv(responses);
+  it('Fetch errors are handled and listeners triggered and disposed', async () => {
+    const responses: MockedResponse[] = [
+      { errorType: 'graphQLError' },
+      { errorType: 'networkError' },
+    ];
+    const { result, waitForErrorChange } = createTestEnv(responses);
     let context = result.current;
+    const errorListener = jest.fn();
+    const errorListener2 = jest.fn();
     await act(async () => {
       // first promise is resolved when context.loading changes to true
-      const loadingPromise = waitForUpdate();
+      const errorPromise = waitForErrorChange();
+      const listenerDisposer = context.addErrorListener(errorListener);
+      context.addErrorListener(errorListener2);
       context.fetch();
-      await loadingPromise;
-      // second promise is resolved when context.loading changes to false and error is returned
-      const dataLoadedPromise = waitForUpdate();
-      await dataLoadedPromise;
+      await errorPromise;
       context = result.current;
       expect(context.data).toBeUndefined();
       expect(context.error).toBeDefined();
       expect(context.loading).toEqual(false);
       expect(context.isInitialized).toEqual(true);
       expect(context.isComplete).toEqual(false);
+      await waitFor(() => {
+        expect(errorListener.mock.calls.length).toEqual(1);
+        expect(errorListener2.mock.calls.length).toEqual(1);
+      });
+      listenerDisposer();
+      const secondErrorPromise = waitForErrorChange();
+      context.refetch();
+      await secondErrorPromise;
+      await waitFor(() => {
+        expect(errorListener2.mock.calls.length).toEqual(2);
+      });
+      expect(errorListener.mock.calls.length).toEqual(1);
     });
   });
 });
