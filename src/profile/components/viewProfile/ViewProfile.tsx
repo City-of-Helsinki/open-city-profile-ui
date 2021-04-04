4bb50fdd8d285e9b424a65c1d41d61e459c46c68
NikoHelle <niko.helle@hyperactive.fi>
2021-04-04 18:23:43 +0300

    Use new ProfileContext in Components
    Added ProfileContext Provider to App.tsx and then the context can be consumed by child components.
    
    ProfileInformation has only placeholder for new components as old ones do not use Context.
    
    ViewProfile in never rendered if Profile (parent) does not have data so ViewProfile do not need fetch or error handling.
    
    Added new color that is not in HDS.

--- src/profile/components/viewProfile/ViewProfile.tsx
+++ src/profile/components/viewProfile/ViewProfile.tsx
@@ -1,48 +1,27 @@
-import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
-import { loader } from 'graphql.macro';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Switch, Route } from 'react-router-dom';
-import * as Sentry from '@sentry/browser';
 import { Section } from 'hds-react';
 
 import styles from './ViewProfile.module.css';
 import responsive from '../../../common/cssHelpers/responsive.module.css';
 import PageHeading from '../../../common/pageHeading/PageHeading';
 import ProfileInformation from '../profileInformation/ProfileInformation';
-import EditProfile from '../editProfile/EditProfile';
-import getNicknameOrName from '../../helpers/getNicknameOrName';
 import ServiceConnections from '../serviceConnections/ServiceConnections';
 import Subscriptions from '../../../subscriptions/components/subsciptions/Subscriptions';
-import { MyProfileQuery } from '../../../graphql/generatedTypes';
 import Explanation from '../../../common/explanation/Explanation';
-import useToast from '../../../toast/useToast';
-
-const MY_PROFILE = loader('../../graphql/MyProfileQuery.graphql');
+import { ProfileContext } from '../../context/ProfileContext';
 
 function ViewProfile(): React.ReactElement {
-  const [isEditing, setEditing] = useState(false);
   const { t } = useTranslation();
-  const { createToast } = useToast();
-
-  const { data, loading } = useQuery<MyProfileQuery>(MY_PROFILE, {
-    errorPolicy: 'ignore',
-    onError: (error: Error) => {
-      Sentry.captureException(error);
-      createToast({ type: 'error' });
-    },
-  });
-
-  const toggleEditing = () => {
-    setEditing(prevState => !prevState);
-  };
+  const { isComplete, getName } = useContext(ProfileContext);
 
   return (
     <div className={styles.viewProfile}>
-      {data && (
+      {isComplete && (
         <React.Fragment>
           <Section korosType="basic">
-            <PageHeading text={getNicknameOrName(data)} />
+            <PageHeading text={getName(true)} />
           </Section>
           <div className={styles.contentWrapper}>
             <Switch>
@@ -60,20 +39,9 @@ function ViewProfile(): React.ReactElement {
                     <Explanation
                       main={t('profileInformation.title')}
                       titleVariant="h2"
+                      small={t('profileInformation.description')}
                     />
-                    {!isEditing ? (
-                      <ProfileInformation
-                        data={data}
-                        loading={loading}
-                        isEditing={isEditing}
-                        setEditing={toggleEditing}
-                      />
-                    ) : (
-                      <EditProfile
-                        setEditing={toggleEditing}
-                        profileData={data}
-                      />
-                    )}
+                    <ProfileInformation />
                   </div>
                 </div>
               </Route>
