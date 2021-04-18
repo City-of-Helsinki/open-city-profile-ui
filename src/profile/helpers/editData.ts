857a615a841381330e2271b0b410f47f124b6e91
NikoHelle <niko.helle@hyperactive.fi>
2021-04-18 22:02:31 +0300

    Added data updating to editData.ts
    Renamed 'create' to 'createNewItem' to make it cleared what it creates
    
    New functions:
    - updateItemAndCreateSaveData: called when saving. It updates passed item by cloning it and all other items too. Value of the cloned item is set as passed new value. Function returns a formValue object that is passed on to updateMutationVariables which creates the update variables. While saving the item may not change and "preventDoubleEdits" -function checks that.
    
    - updateData: called after save. If editData is waiting for an update (any item.saving is set). Items are updated if new profile data has same value edit data does.
    
    -updateAfterSavingError: when save error occurs, item.saving is set to "undefined"
    
    -updateItemAndCloneList: clones item, updates clone.value and returns all items as new, cloned list
    
    -hasItemUpdated: returns true/false, depending on whether prop that is being saved (value/primary) matches the same prop in profile
    
    -updateItems: updates items being saved if hasItemUpdated() returns true. Tracks also the new item (even they cannot be created yet). There can be only one new item at a time, so once it's found in profile data, no need to check new items anymore. Returns new list only if any items where updated.

--- src/profile/helpers/editData.ts
+++ src/profile/helpers/editData.ts
@@ -11,6 +11,7 @@ import {
   PrimaryPhone,
   Language,
 } from '../../graphql/typings';
+import { formConstants } from '../constants/formConstants';
 import getAddressesFromNode from '../helpers/getAddressesFromNode';
 import getEmailsFromNode from '../helpers/getEmailsFromNode';
 import getPhonesFromNode from '../helpers/getPhonesFromNode';
@@ -71,7 +72,13 @@ export type FormValues = {
   phones: PhoneNode[];
 };
 
-type SaveType = 'value' | 'set-primary' | 'remove' | undefined;
+export const saveTypeSetPrimary = 'set-primary';
+
+export type SaveType =
+  | 'value'
+  | typeof saveTypeSetPrimary
+  | 'remove'
+  | undefined;
 
 export type EditData = {
   readonly id: string;
@@ -83,12 +90,26 @@ export type EditData = {
 export type EditFunctions = {
   create: (newProfileData: EditDataProfileSource) => EditData;
   getEditData: () => EditData[];
+  updateItemAndCreateSaveData: (
+    targetItem: EditData,
+    newValue: EditDataValue
+  ) => Partial<FormValues>;
+  updateData: (newProfileRoot: ProfileRoot) => boolean;
+  updateAfterSavingError: (id: string) => boolean;
 };
 
 function isMultiItemDataType(dataType: EditDataType): boolean {
   return !(
     dataType === basicDataType || dataType === additionalInformationType
   );
+}
+
+function isSaving(allItems: EditData[]): boolean {
+  return !!allItems.find(item => !!item.saving);
+}
+
+function getNewItem(allItems: EditData[]): EditData | undefined {
+  return allItems.find(item => item.id === '');
 }
 
 function getValueProps(dataType: EditDataType): string[] {
@@ -105,7 +126,7 @@ function getValueProps(dataType: EditDataType): string[] {
   }
 }
 
-function pickValue(
+export function pickValue(
   profileDataItem: EditDataProfileSource,
   dataType: EditDataType
 ): EditDataValue {
@@ -113,14 +134,14 @@ function pickValue(
   return _.pick(profileDataItem, pickProps) as EditDataValue;
 }
 
-function create(
+function createNewItem(
   profileData: EditDataProfileSource,
   dataType: EditDataType,
   overrides?: { value?: EditDataValue; saving?: SaveType }
 ): EditData {
   return {
     id: profileData.id,
-    primary: !!(profileData as MultiItemProfileNode).primary,
+    primary: (profileData as MultiItemProfileNode).primary,
     value: pickValue(profileData, dataType),
     saving: undefined,
     ...overrides,
@@ -150,17 +171,211 @@ export function pickSources(
   }
 }
 
+function cloneAndMutateItem(
+  data: EditData,
+  overrides?: Partial<EditData>
+): EditData {
+  return {
+    ...data,
+    ...overrides,
+  };
+}
+
+function findItemIndex(
+  allItems: EditData[],
+  idOrEditData: string | EditData
+): number {
+  const itemId =
+    typeof idOrEditData === 'string' ? idOrEditData : idOrEditData.id;
+  return allItems.findIndex(item => itemId === item.id);
+}
+
+function findItem(allItems: EditData[], id: string): EditData | undefined {
+  return allItems.find(item => id === item.id);
+}
+
+function updateItemAndCloneList(
+  allItems: EditData[],
+  item: EditData,
+  newValue: EditDataValue,
+  saving?: SaveType
+): EditData[] {
+  const index = findItemIndex(allItems, item.id);
+  if (index < 0) {
+    throw new Error('Item not found in updateEditData() ');
+  }
+  const updatedItem = cloneAndMutateItem(item, {
+    value: newValue,
+    saving: saving || undefined,
+  });
+  const newList = _.cloneDeep(allItems);
+  newList[index] = updatedItem;
+  return newList;
+}
+
+export function createNewProfileNode<T extends MultiItemProfileNode>(
+  dataType: EditDataType,
+  overrides?: Partial<T>
+): T {
+  return {
+    ...(formConstants.EMPTY_VALUES[dataType] as T),
+    ...overrides,
+  };
+}
+
+function createFormValues(
+  allItems: EditData[],
+  dataType: EditDataType
+): Partial<FormValues> {
+  if (!isMultiItemDataType(dataType)) {
+    const value = allItems[0].value as
+      | BasicDataValue
+      | AdditionalInformationValue;
+    return {
+      ...value,
+    };
+  } else {
+    const nodes = allItems
+      .filter(item => item.saving !== 'remove')
+      .map(item =>
+        createNewProfileNode(dataType, {
+          ...item.value,
+          primary: item.primary,
+        })
+      );
+    return {
+      [dataType]: nodes,
+    };
+  }
+}
+
+function hasItemUpdated(
+  item: EditData,
+  dataType: EditDataType,
+  profileData: EditDataProfileSource
+): boolean {
+  const { saving } = item;
+  if (!saving) {
+    return false;
+  }
+  if (saving === 'value') {
+    const profileDataValue = pickValue(profileData, dataType) as BasicDataValue;
+    return _.isEqual(item.value, profileDataValue);
+  }
+  if (saving === saveTypeSetPrimary) {
+    return _.isEqual(
+      item.primary,
+      (profileData as MultiItemProfileNode).primary
+    );
+  }
+  return false;
+}
+
+export function updateItems(
+  allItems: EditData[],
+  profileDataItems: EditDataProfileSource[],
+  dataType: EditDataType
+): EditData[] | null {
+  let dataHasUpdated = false;
+  const newList: EditData[] = [];
+  let newItem = getNewItem(allItems);
+  profileDataItems.forEach(profileDataItem => {
+    const existingItem = findItem(allItems, profileDataItem.id);
+    const newValue = pickValue(profileDataItem, dataType);
+    if (existingItem) {
+      const itemHasUpdated = hasItemUpdated(
+        existingItem,
+        dataType,
+        profileDataItem
+      );
+      if (itemHasUpdated) {
+        const copy = createNewItem(profileDataItem, dataType, {
+          saving: undefined,
+        });
+        newList.push(copy);
+        dataHasUpdated = true;
+      } else {
+        newList.push(existingItem);
+      }
+    } else {
+      if (newItem && _.isEqual(newItem.value, newValue)) {
+        dataHasUpdated = true;
+        newItem = undefined;
+      }
+      const copy = createNewItem(profileDataItem, dataType);
+      newList.push(copy);
+    }
+  });
+  if (newItem) {
+    newList.push(newItem);
+  }
+  if (!dataHasUpdated && allItems.length !== profileDataItems.length) {
+    dataHasUpdated = true;
+  }
+  return dataHasUpdated ? newList : null;
+}
+
 export function createEditorForDataType(
   profileRoot: ProfileRoot,
   dataType: EditDataType
 ): EditFunctions {
   const profileData = profileRoot.myProfile as ProfileData;
   const profileDataSources = pickSources(profileData, dataType);
-  const allItems: EditData[] = profileDataSources.map(source =>
-    create(source, dataType)
+  let allItems: EditData[] = profileDataSources.map(source =>
+    createNewItem(source, dataType)
   );
+  const preventDoubleEdits = (item: EditData): void => {
+    if (item.saving) {
+      throw new Error(
+        'Data is being saved. Cannot edit before save is complete'
+      );
+    }
+  };
   return {
-    create: newProfileData => create(newProfileData, dataType),
+    create: newProfileData => createNewItem(newProfileData, dataType),
     getEditData: () => allItems,
+    updateItemAndCreateSaveData: (targetItem, newValue) => {
+      preventDoubleEdits(targetItem);
+      allItems = updateItemAndCloneList(
+        allItems,
+        targetItem,
+        newValue,
+        'value'
+      );
+      return createFormValues(allItems, dataType);
+    },
+    updateData: (newProfileRoot: ProfileRoot) => {
+      const newSources = pickSources(
+        newProfileRoot.myProfile as ProfileData,
+        dataType
+      );
+      if (!newSources.length) {
+        if (allItems.length) {
+          allItems = [];
+          return true;
+        }
+        return false;
+      }
+      if (!isSaving(allItems)) {
+        return false;
+      }
+
+      const newList = updateItems(allItems, newSources, dataType);
+      if (newList) {
+        allItems = newList;
+      }
+      return !!newList;
+    },
+    updateAfterSavingError: id => {
+      const targetItem = findItem(allItems, id);
+      if (!targetItem) {
+        throw new Error('Target not found for updateAfterSavingError() ');
+      }
+      if (!targetItem.saving) {
+        return false;
+      }
+      allItems = updateItemAndCloneList(allItems, targetItem, targetItem.value);
+      return true;
+    },
   };
 }
