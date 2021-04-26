7ea40e655091f0423a7b986b7787f0cf2be99a19
NikoHelle <niko.helle@hyperactive.fi>
2021-04-26 12:24:06 +0300

    Render and edit addresses, emails and phones
    Components for handling multiItemProfileData.
    
    MultiItemEditor handles rendering of item lists  and also handles common actions like saving and renders save indicators. It also shows button for adding new items.
    
    MultiItemRow renders emails and phones, because those have same number of fields and are simple to handle.
    
    MultiItemAddressRow is almost identical but different enough to have its own component for handling addresses
    
    MultiItem*Row are like BasicData.tsx, but also handle new items.
    
    useCommonEditHandling is used by MultiItem*Row because they have identical edit functions.
    
    Remove and set-primary -buttons are not rendered at the moment.

---
+++ src/profile/components/multiItemRow/MultiItemRow.tsx
@@ -0,0 +1,125 @@
+import { TextInput } from 'hds-react';
+import React from 'react';
+import { Field, Formik, FormikProps, Form } from 'formik';
+import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
+
+import styles from './multiItemRow.module.css';
+import { EditDataValue, EmailValue, PhoneValue } from '../../helpers/editData';
+import { createFormFieldHelpers } from '../../helpers/formik';
+import { phoneSchema, emailSchema } from '../../../common/schemas/schemas';
+import FormButtons from '../formButtons/FormButtons';
+import EditButtons from '../editButtons/EditButtons';
+import commonFormStyles from '../../../common/cssHelpers/form.module.css';
+import SaveIndicator from '../saveIndicator/SaveIndicator';
+import { useCommonEditHandling } from '../../hooks/useCommonEditHandling';
+import { RowItemProps } from '../multiItemEditor/MultiItemEditor';
+import { getFormFields } from '../../helpers/formProperties';
+
+type EmailAndPhoneFormikValue = { value: string };
+
+function MultiItemRow(props: RowItemProps): React.ReactElement {
+  const {
+    data: { value, primary },
+    testId,
+    dataType,
+  } = props;
+  const { t } = useTranslation();
+  const {
+    isEditing,
+    currentAction,
+    actionHandler,
+    isNew,
+  } = useCommonEditHandling(props);
+  const schema = dataType === 'phones' ? phoneSchema : emailSchema;
+  const inputValue: string =
+    (dataType === 'phones'
+      ? (value as PhoneValue).phone
+      : (value as EmailValue).email) || '';
+
+  const inputId = `${testId}-value`;
+  const formFields = getFormFields(dataType);
+  const { hasFieldError, getFieldErrorMessage } = createFormFieldHelpers<
+    EmailAndPhoneFormikValue
+  >(t, isNew);
+  const convertFormPropsToEditDataValue = (
+    formValues: EmailAndPhoneFormikValue
+  ): Partial<EditDataValue> => {
+    const formValue = formValues.value;
+    const propName = dataType === 'phones' ? 'phone' : 'email';
+    return {
+      [propName]: formValue,
+    };
+  };
+  if (isEditing) {
+    return (
+      <div
+        className={classNames([
+          commonFormStyles.contentWrapper,
+          styles.rowContentWrapper,
+        ])}
+      >
+        <Formik
+          initialValues={{
+            value: inputValue,
+          }}
+          onSubmit={async values => {
+            const editDataValues = convertFormPropsToEditDataValue(values);
+            await actionHandler('save', editDataValues);
+          }}
+          validationSchema={schema}
+        >
+          {(formikProps: FormikProps<EmailAndPhoneFormikValue>) => (
+            <Form>
+              <div className={styles.editableRow}>
+                <Field
+                  name="value"
+                  id={inputId}
+                  maxLength={formFields.value.max as number}
+                  as={TextInput}
+                  invalid={hasFieldError(formikProps, 'value')}
+                  aria-invalid={hasFieldError(formikProps, 'value')}
+                  helperText={getFieldErrorMessage(formikProps, 'value')}
+                  aria-labelledby={`${dataType}-value-helper`}
+                  autoFocus
+                />
+                <FormButtons
+                  handler={actionHandler}
+                  disabled={!!currentAction}
+                  testId={testId}
+                />
+              </div>
+              <SaveIndicator action={currentAction} testId={testId} />
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
+  return (
+    <div
+      className={classNames([
+        commonFormStyles.contentWrapper,
+        styles.rowContentWrapper,
+      ])}
+    >
+      <span className={styles.value} data-testid={`${testId}-value`}>
+        {inputValue || '–'}
+      </span>
+      <EditButtons
+        handler={actionHandler}
+        actions={{
+          removable: false,
+          primary,
+          setPrimary: false,
+        }}
+        editButtonId={`${testId}-edit-button`}
+        disabled={!!currentAction}
+        testId={testId}
+      />
+      <SaveIndicator action={currentAction} testId={testId} />
+    </div>
+  );
+}
+
+export default MultiItemRow;
