32f690c7743d57131380301041e699334c30ad24
NikoHelle <niko.helle@hyperactive.fi>
2021-08-23 13:21:14 +0300

    (HP-896) Use new env - from window._env_, not process.env

--- src/gdprApi/gdprAuthCodeManager.ts
+++ src/gdprApi/gdprAuthCodeManager.ts
@@ -1,7 +1,7 @@
 import GdprAuthorizationTokenManager from './GdprAuthorizationCodeManager';
 
 const gdprAuthCodeManager = new GdprAuthorizationTokenManager({
-  clientId: process.env.REACT_APP_PROFILE_AUDIENCE as string,
+  clientId: window._env_.REACT_APP_PROFILE_BE_GDPR_CLIENT_ID as string,
   redirectUri: `${window.location.origin}/gdpr-callback`,
   oidcAuthority: window._env_.REACT_APP_OIDC_AUTHORITY as string,
 });
