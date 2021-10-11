adc604a2621bc9dae963cd9e0ca3be8ad76b9674
NikoHelle <niko.helle@hyperactive.fi>
2021-10-11 16:26:22 +0300

    (HP-1012) Use TopSectionWithKoros and common css in ViewProfile.tsx
    Removed css styles that are inherited from common css or conflict with them
    
    Use common naming with class names: content" instead of "content-wrapper"

--- src/profile/components/viewProfile/ViewProfile.tsx
+++ src/profile/components/viewProfile/ViewProfile.tsx
@@ -1,52 +1,55 @@
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Switch, Route } from 'react-router-dom';
-import { Section } from 'hds-react';
+import classNames from 'classnames';
 
 import styles from './ViewProfile.module.css';
-import responsive from '../../../common/cssHelpers/responsive.module.css';
 import PageHeading from '../../../common/pageHeading/PageHeading';
 import ProfileInformation from '../profileInformation/ProfileInformation';
 import ServiceConnections from '../serviceConnections/ServiceConnections';
 import Explanation from '../../../common/explanation/Explanation';
 import { ProfileContext } from '../../context/ProfileContext';
+import commonContentStyles from '../../../common/cssHelpers/content.module.css';
+import TopSectionWithKoros from '../../../common/topSectionWithKoros/TopSectionWithKoros';
 
 function ViewProfile(): React.ReactElement {
   const { t } = useTranslation();
   const { isComplete, getName } = useContext(ProfileContext);
 
   return (
-    <div className={styles['view-profile']}>
+    <React.Fragment>
       {isComplete && (
         <React.Fragment>
-          <Section korosType="basic">
+          <TopSectionWithKoros>
             <PageHeading
               text={getName(true)}
               dataTestId="view-profile-heading"
             />
-          </Section>
-          <div className={styles['content-wrapper']}>
+          </TopSectionWithKoros>
+          <div
+            className={classNames([
+              commonContentStyles['common-content-area'],
+              commonContentStyles['common-bottom-padding'],
+              styles['content'],
+            ])}
+          >
             <Switch>
               <Route path="/connected-services">
                 <ServiceConnections />
               </Route>
               <Route path="/">
-                <div className={styles['profile-content']}>
-                  <div className={responsive['max-width-centered']}>
-                    <Explanation
-                      main={t('profileInformation.title')}
-                      titleVariant="h2"
-                      small={t('profileInformation.description')}
-                    />
-                    <ProfileInformation />
-                  </div>
-                </div>
+                <Explanation
+                  main={t('profileInformation.title')}
+                  titleVariant="h2"
+                  small={t('profileInformation.description')}
+                />
+                <ProfileInformation />
               </Route>
             </Switch>
           </div>
         </React.Fragment>
       )}
-    </div>
+    </React.Fragment>
   );
 }
 
