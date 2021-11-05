8c85b0c49cb09ed867a3b4090430842c2039263f
NikoHelle <niko.helle@hyperactive.fi>
2021-11-05 10:26:09 +0200

    (HP-1049) Refactored ConfirmationModel tests
    Use common test function to create selectors and removed useless result rendering
    
    Use also actual translations

--- src/profile/components/modals/confirmationModal/__tests__/ConfirmationModal.test.tsx
+++ src/profile/components/modals/confirmationModal/__tests__/ConfirmationModal.test.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { fireEvent, render, RenderResult } from '@testing-library/react';
-import to from 'await-to-js';
+import { render } from '@testing-library/react';
 
 import ConfirmationModal, {
   Props as ModalComponentProps,
 } from '../ConfirmationModal';
+import { createDomHelpersWithTesting } from '../../../../../common/test/testingLibraryTools';
+import i18n from '../../../../../common/test/testi18nInit';
+
+type RenderProps = Pick<ModalComponentProps, 'content' | 'title'>;
 
 describe('<ConfirmationModal /> ', () => {
   const modalId = 'confirmation-modal';
@@ -18,10 +21,9 @@ describe('<ConfirmationModal /> ', () => {
   };
   const onClose = jest.fn();
   const onConfirm = jest.fn();
-  const renderModal = ({
-    content,
-    title,
-  }: Pick<ModalComponentProps, 'content' | 'title'>) =>
+  const t = i18n.getFixedT('fi');
+
+  const renderModal = ({ content, title }: RenderProps) =>
     render(
       <ConfirmationModal
         onClose={onClose}
@@ -33,35 +35,20 @@ describe('<ConfirmationModal /> ', () => {
       />
     );
 
-  const createDomHelpersWithTesting = (renderResult: RenderResult) => ({
-    findByTestId: async (testId: string): Promise<HTMLElement | null> => {
-      const [, el] = await to(renderResult.findByTestId(testId));
-      return Promise.resolve(el || null);
-    },
-    findById: async (id: string): Promise<HTMLElement | null> => {
-      const el = renderResult.baseElement.querySelector(`#${id}`);
-      return Promise.resolve((el as HTMLElement) || null);
-    },
-    click: async (target: HTMLElement) => {
-      expect(!!target).toBeTruthy();
-      fireEvent.click(target as Element);
-      Promise.resolve();
-    },
-  });
+  const renderAndReturnHelpers = (props: RenderProps) =>
+    createDomHelpersWithTesting(renderModal(props));
 
   beforeEach(() => jest.resetAllMocks());
 
   it('Renders title when defined', async () => {
     const props = { title: 'THIS_IS_TITLE' };
-    const result = renderModal(props);
-    const { findById } = createDomHelpersWithTesting(result);
+    const { findById } = renderAndReturnHelpers(props);
     const title = (await findById(testIds.title)) as HTMLElement;
     expect(title).not.toBeNull();
     expect(title.innerHTML.includes(props.title)).toBeTruthy();
   });
   it('Does not render title when not defined', async () => {
-    const result = renderModal({});
-    const { findById } = createDomHelpersWithTesting(result);
+    const { findById } = renderAndReturnHelpers({});
     const title = await findById(testIds.title);
     expect(title).toBeNull();
   });
@@ -69,16 +56,16 @@ describe('<ConfirmationModal /> ', () => {
     const ContentWithTestId = () => (
       <div data-testid={testIds.content}>THIS_IS_CONTENT</div>
     );
-    const result = renderModal({ content: () => <ContentWithTestId /> });
-    const { findByTestId, findById } = createDomHelpersWithTesting(result);
+    const { findByTestId, findById } = renderAndReturnHelpers({
+      content: () => <ContentWithTestId />,
+    });
     const content = await findByTestId(testIds.content);
     expect(content).not.toBeNull();
     const description = await findById(testIds.description);
     expect(description).not.toBeNull();
   });
   it('Does not render content when not defined', async () => {
-    const result = renderModal({});
-    const { findByTestId } = createDomHelpersWithTesting(result);
+    const { findByTestId } = renderAndReturnHelpers({});
     const content = await findByTestId(testIds.content);
     expect(content).toBeNull();
   });
@@ -86,8 +73,9 @@ describe('<ConfirmationModal /> ', () => {
     const ContentWithTestId = () => (
       <div data-testid={testIds.content}>THIS_IS_CONTENT</div>
     );
-    const result = renderModal({ content: () => <ContentWithTestId /> });
-    const { findByTestId, click } = createDomHelpersWithTesting(result);
+    const { findByTestId, click } = renderAndReturnHelpers({
+      content: () => <ContentWithTestId />,
+    });
     const confirmButton = (await findByTestId(testIds.confirm)) as HTMLElement;
     expect(confirmButton.innerHTML.includes(actionButtonText)).toBeTruthy();
     await click(confirmButton);
@@ -98,10 +86,13 @@ describe('<ConfirmationModal /> ', () => {
     const ContentWithTestId = () => (
       <div data-testid={testIds.content}>THIS_IS_CONTENT</div>
     );
-    const result = renderModal({ content: () => <ContentWithTestId /> });
-    const { findByTestId, click } = createDomHelpersWithTesting(result);
+    const { findByTestId, click } = renderAndReturnHelpers({
+      content: () => <ContentWithTestId />,
+    });
     const cancelButton = (await findByTestId(testIds.cancel)) as HTMLElement;
-    expect(cancelButton.innerHTML.includes('cancel')).toBeTruthy();
+    expect(
+      cancelButton.innerHTML.includes(t('confirmationModal.cancel'))
+    ).toBeTruthy();
     await click(cancelButton);
     expect(onClose.mock.calls).toHaveLength(1);
     expect(onConfirm.mock.calls).toHaveLength(0);
