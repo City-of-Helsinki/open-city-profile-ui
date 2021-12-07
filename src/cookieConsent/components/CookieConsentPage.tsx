cf189330519a1bbaf0e314250d2af4656b7ebad7
NikoHelle <niko.helle@hyperactive.fi>
2021-12-07 11:51:42 +0200

    (HP-962) Added cookie conset settings page

---
+++ src/cookieConsent/components/CookieConsentPage.tsx
@@ -0,0 +1,87 @@
+import classNames from 'classnames';
+import { Notification } from 'hds-react';
+import React, { useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import commonContentStyles from '../../common/cssHelpers/content.module.css';
+import PageLayout from '../../common/pageLayout/PageLayout';
+import { ConsentController } from '../cookieConsentController';
+import styles from './CookieConsent.module.css';
+import { CookieConsentContext } from './CookieConsentContext';
+import CookieDetails from './CookieDetails';
+
+function CookieConsentPage(): React.ReactElement | null {
+  const cookieConsentContext = useContext(CookieConsentContext);
+
+  const { t } = useTranslation();
+
+  const [, forceUpdate] = useState<number>(0);
+
+  const [showSaveNotification, setShowSaveNotification] = useState<boolean>(
+    false
+  );
+
+  const reRender = () => {
+    forceUpdate(p => p + 1);
+  };
+
+  const save = (): void => {
+    cookieConsentContext.save();
+    if (cookieConsentContext.hasUserHandledAllConsents()) {
+      setShowSaveNotification(true);
+    }
+  };
+
+  const approveRequired: ConsentController['approveRequired'] = () => {
+    cookieConsentContext.approveRequired();
+    save();
+    reRender();
+  };
+
+  const onChange: ConsentController['update'] = (key, value) => {
+    cookieConsentContext.update(key, value);
+    reRender();
+  };
+
+  return (
+    <PageLayout title={'cookies.cookieSettings'}>
+      <div
+        className={classNames([
+          commonContentStyles['common-content-area'],
+          commonContentStyles['common-bottom-padding'],
+          styles['settings-page-content'],
+        ])}
+        data-testid="cookie-consent-settings"
+      >
+        <div className={styles.content}>
+          <div className={styles['text-content']}>
+            <h1>{t('cookies.cookieSettings')}</h1>
+            <p id="cookie-consent-description">
+              {t('cookies.informationText')}
+            </p>
+          </div>
+
+          <CookieDetails
+            requiredConsents={cookieConsentContext.getRequired()}
+            optionalConsents={cookieConsentContext.getOptional()}
+            onChange={onChange}
+            onClose={() => undefined}
+            onApproveSelectionsAndRequired={() => {
+              approveRequired();
+            }}
+            isOnSettingsPage
+          />
+          {showSaveNotification && (
+            <Notification
+              type={'success'}
+              label={t('cookies.consentSettingsSaved')}
+              className={styles['notification']}
+            />
+          )}
+        </div>
+      </div>
+    </PageLayout>
+  );
+}
+
+export default CookieConsentPage;
--- src/cookieConsent/components/CookieDetails.tsx
+++ src/cookieConsent/components/CookieDetails.tsx
@@ -14,35 +14,48 @@ export type CookieDetailProps = {
   onChange: ConsentController['update'];
   onClose: () => void;
   onApproveSelectionsAndRequired: () => void;
+  isOnSettingsPage?: boolean;
 };
 
 function CookieDetails({
   onClose,
-  requiredConsents,
   optionalConsents,
   onChange,
   onApproveSelectionsAndRequired,
+  isOnSettingsPage,
 }: CookieDetailProps): React.ReactElement {
   const { t } = useTranslation();
+  const Heading = ({
+    level,
+    children,
+  }: {
+    level: number;
+    children: React.ReactNode;
+  }) => {
+    if (isOnSettingsPage) {
+      return level === 1 ? <h2>{children}</h2> : <h3>{children}</h3>;
+    }
+    return (
+      <span
+        className={styles[`emulated-h${level}`]}
+        role="heading"
+        aria-level={level}
+        id="cookie-consent-active-heading"
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+        tabIndex={0}
+      >
+        {children}
+      </span>
+    );
+  };
   return (
     <div
       className={styles['text-content']}
       data-testid="cookie-consent-details"
     >
-      <span
-        className={styles['emulated-h1']}
-        role="heading"
-        aria-level={1}
-        id="cookie-consent-active-heading"
-        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-        tabIndex={0}
-      >
-        {t('cookies.detailsTitle')}
-      </span>
+      <Heading level={1}>{t('cookies.detailsTitle')}</Heading>
       <p>{t('cookies.detailsText')}</p>
-      <span className={styles['emulated-h2']} role="heading" aria-level={2}>
-        {t('cookies.requiredConsentsTitle')}
-      </span>
+      <Heading level={2}>{t('cookies.requiredConsentsTitle')}</Heading>
       <p>{t('cookies.requiredConsentsText')}</p>
       <RequiredCookieInformation consentList={getConsentInfo('required', t)} />
       {optionalConsents && (
@@ -65,15 +78,16 @@ function CookieDetails({
           </Button>
         </React.Fragment>
       )}
-
-      <p>
-        <button
-          className={styles['plain-text-button']}
-          onClick={() => onClose()}
-        >
-          {t('cookies.backToBeginning')}
-        </button>
-      </p>
+      {!isOnSettingsPage && (
+        <p>
+          <button
+            className={styles['plain-text-button']}
+            onClick={() => onClose()}
+          >
+            {t('cookies.backToBeginning')}
+          </button>
+        </p>
+      )}
     </div>
   );
 }
