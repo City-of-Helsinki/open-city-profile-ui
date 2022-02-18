d255dfe78860453cb085bdfd1a60b426cf895fb5
NikoHelle <niko.helle@hyperactive.fi>
2022-02-18 14:42:28 +0200

    (HP-1067) Tests for error notification, load indicator and refetch

--- src/profile/components/serviceConnections/__tests__/ServiceConnections.test.tsx
+++ src/profile/components/serviceConnections/__tests__/ServiceConnections.test.tsx
@@ -42,12 +42,13 @@ describe('<ServiceConnections />', () => {
     resetApolloMocks();
   });
 
-  it('should render all service connections', async () => {
+  it('should render all service connections. A load indicator is shown while loading', async () => {
     const responses: MockedResponse[] = [
       { profileDataWithServiceConnections: queryResultWithServiceConnection },
     ];
     await act(async () => {
-      const { getElement } = await renderTestSuite(responses);
+      const { getElement, waitForElement } = await renderTestSuite(responses);
+      await waitForElement({ testId: 'load-indicator' });
       await waitFor(() =>
         serviceList.forEach(service => {
           expect(getElement({ text: service.title as string })).toBeDefined();
@@ -55,6 +56,7 @@ describe('<ServiceConnections />', () => {
       );
     });
   });
+
   it('should render specific text if there are no service connections', async () => {
     const t = i18n.getFixedT('fi');
     const responses: MockedResponse[] = [
@@ -71,6 +73,7 @@ describe('<ServiceConnections />', () => {
       });
     });
   });
+
   it('should send current language as a variable. Value must be in uppercase', async () => {
     const responses: MockedResponse[] = [
       { profileDataWithServiceConnections: queryResultWithServiceConnection },
@@ -82,6 +85,35 @@ describe('<ServiceConnections />', () => {
       await waitForElement({ text: serviceList[0].title as string });
       expect(queryVariableTracker).toHaveBeenCalledWith({
         language: lang.toUpperCase(),
+      });
+    });
+  });
+
+  it('should render a notification with reload button when error occurs. Button click refetches data.', async () => {
+    const responses: MockedResponse[] = [
+      {
+        errorType: 'graphQLError',
+      },
+      { profileDataWithServiceConnections: queryResultWithServiceConnection },
+    ];
+    const t = i18n.getFixedT('fi');
+    await act(async () => {
+      const {
+        getElement,
+        waitForElement,
+        clickElement,
+      } = await renderTestSuite(responses);
+
+      await waitForElement({ testId: 'service-connections-load-error' });
+
+      await clickElement({
+        querySelector: '[data-testid="service-connections-load-error"] button',
+      });
+
+      await waitFor(() => {
+        expect(
+          getElement({ text: t('serviceConnections.title') })
+        ).toBeDefined();
       });
     });
   });
