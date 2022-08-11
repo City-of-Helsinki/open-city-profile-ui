2c0521837b88b53fda723fbc6728a4e843d8fab6
Niko Helle <niko.hele@hyperactive.fi>
2022-08-11 15:34:09 +0300

    (HP-1526) Change bg colors and component alignment
    White on white was removed. The content area gets a litte wider as box padding is gone.
    
    ProfileInformation bottom area needs to be grey all the way to the sides.
    
    Removed user name with koros
    
    To fill full width of the page, the content padding must be moved inside components. And components must be wrapped.
    
    So ViewProfile has no css or extra elements anymore and those are moved to ServiceConnections and ProfileInformation.
    
    Fixed tests. User name in "view-profile-heading" was removed and caused failures. Added new testIds.

--- src/profile/components/profileInformation/ProfileInformation.tsx
+++ src/profile/components/profileInformation/ProfileInformation.tsx
@@ -1,4 +1,7 @@
 import React, { Fragment, useContext } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import classNames from 'classnames';
+import { Link } from 'hds-react';
 
 import DeleteProfile from '../deleteProfile/DeleteProfile';
 import DownloadData from '../downloadData/DownloadData';
@@ -10,28 +13,62 @@ import MultiItemEditor from '../multiItemEditor/MultiItemEditor';
 import AdditionalInformation from '../additionalInformation/AdditionalInformation';
 import VerifiedPersonalInformation from '../verifiedPersonalInformation/VerifiedPersonalInformation';
 import getVerifiedPersonalInformation from '../../helpers/getVerifiedPersonalInformation';
+import Explanation from '../../../common/explanation/Explanation';
+import commonContentStyles from '../../../common/cssHelpers/content.module.css';
 
 function ProfileInformation(): React.ReactElement {
   const { data } = useContext(ProfileContext);
+  const { t } = useTranslation();
   const hasVerifiedData = !!getVerifiedPersonalInformation(data);
   const UserDataComponent = hasVerifiedData
     ? VerifiedPersonalInformation
     : BasicData;
   return (
-    <Fragment>
-      {data && (
-        <Fragment>
-          <UserDataComponent />
-          <EmailEditor />
-          <MultiItemEditor dataType="addresses" />
-          <MultiItemEditor dataType="phones" />
-          <AdditionalInformation />
-        </Fragment>
-      )}
-      <AuthenticationProviderInformation />
-      <DownloadData />
-      <DeleteProfile />
-    </Fragment>
+    <div className={classNames([commonContentStyles['content']])}>
+      <div className={classNames([commonContentStyles['common-content-area']])}>
+        <Explanation
+          heading={t('profileInformation.title')}
+          text={
+            <Trans
+              i18nKey="profileInformation.description"
+              components={{
+                linkToServices: (
+                  <Link href={'/connected-services'} size="M">
+                    {''}
+                  </Link>
+                ),
+                linkToServicesText: t('nav.services'),
+              }}
+            />
+          }
+          dataTestId="profile-information-explanation"
+        />
+        {data && (
+          <Fragment>
+            <UserDataComponent />
+            <EmailEditor />
+            <MultiItemEditor dataType="addresses" />
+            <MultiItemEditor dataType="phones" />
+            <AdditionalInformation />
+          </Fragment>
+        )}
+        <AuthenticationProviderInformation />
+      </div>
+      <div
+        className={classNames([
+          commonContentStyles['content'],
+          commonContentStyles['common-content-area-dark-bg'],
+          commonContentStyles['common-bottom-padding'],
+        ])}
+      >
+        <div
+          className={classNames([commonContentStyles['common-content-area']])}
+        >
+          <DownloadData />
+          <DeleteProfile />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/profile/components/viewProfile/ViewProfile.tsx
+++ src/profile/components/viewProfile/ViewProfile.tsx
@@ -1,62 +1,24 @@
 import React, { useContext } from 'react';
-import { Trans, useTranslation } from 'react-i18next';
-import { Switch, Route, Link } from 'react-router-dom';
-import classNames from 'classnames';
+import { Switch, Route } from 'react-router-dom';
 
-import styles from './ViewProfile.module.css';
-import PageHeading from '../../../common/pageHeading/PageHeading';
 import ProfileInformation from '../profileInformation/ProfileInformation';
 import ServiceConnections from '../serviceConnections/ServiceConnections';
-import Explanation from '../../../common/explanation/Explanation';
 import { ProfileContext } from '../../context/ProfileContext';
-import commonContentStyles from '../../../common/cssHelpers/content.module.css';
-import TopSectionWithKoros from '../../../common/topSectionWithKoros/TopSectionWithKoros';
 
 function ViewProfile(): React.ReactElement {
-  const { t } = useTranslation();
-  const { isComplete, getName } = useContext(ProfileContext);
+  const { isComplete } = useContext(ProfileContext);
 
   return (
     <React.Fragment>
       {isComplete && (
-        <React.Fragment>
-          <TopSectionWithKoros>
-            <PageHeading
-              text={getName(true)}
-              dataTestId="view-profile-heading"
-            />
-          </TopSectionWithKoros>
-          <div
-            className={classNames([
-              commonContentStyles['common-content-area'],
-              commonContentStyles['common-bottom-padding'],
-              styles['content'],
-            ])}
-          >
-            <Switch>
-              <Route path="/connected-services">
-                <ServiceConnections />
-              </Route>
-              <Route path="/">
-                <Explanation
-                  heading={t('profileInformation.title')}
-                  text={
-                    <Trans
-                      i18nKey="profileInformation.description"
-                      components={{
-                        linkToServices: (
-                          <Link to={'/connected-services'}>{''}</Link>
-                        ),
-                        linkToServicesText: t('nav.services'),
-                      }}
-                    />
-                  }
-                />
-                <ProfileInformation />
-              </Route>
-            </Switch>
-          </div>
-        </React.Fragment>
+        <Switch>
+          <Route path="/connected-services">
+            <ServiceConnections />
+          </Route>
+          <Route path="/">
+            <ProfileInformation />
+          </Route>
+        </Switch>
       )}
     </React.Fragment>
   );
