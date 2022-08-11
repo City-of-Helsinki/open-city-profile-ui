afb0c16e0e54ea20dc9fe41085ee67bc5beee924
Niko Helle <niko.hele@hyperactive.fi>
2022-08-11 15:28:57 +0300

    (HP-1526) Unbox DeleteProfile content

--- src/profile/components/deleteProfile/DeleteProfile.tsx
+++ src/profile/components/deleteProfile/DeleteProfile.tsx
@@ -8,7 +8,6 @@ import { useHistory } from 'react-router';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
 
 import ConfirmationModal from '../modals/confirmationModal/ConfirmationModal';
-import ExpandingPanel from '../../../common/expandingPanel/ExpandingPanel';
 import {
   ServiceConnectionsQueryVariables,
   ServiceConnectionsRoot,
@@ -19,14 +18,15 @@ import ModalServicesContent from '../modals/deleteProfileContent/DeleteProfileCo
 import { useFocusSetter } from '../../hooks/useFocusSetter';
 import DeleteProfileError from '../modals/deleteProfileError/DeleteProfileError';
 import createServiceConnectionsQueryVariables from '../../helpers/createServiceConnectionsQueryVariables';
+import ProfileSection from '../../../common/profileSection/ProfileSection';
 
 const SERVICE_CONNECTIONS = loader(
   '../../graphql/ServiceConnectionsQuery.graphql'
 );
 
 function DeleteProfile(): React.ReactElement {
-  const [deleteConfirmationModal, setDeleteConfirmationModal] = useState(false);
-  const [deleteInstructions, setDeleteInstructions] = useState(false);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [isDeleteApproved, setDeleteApproved] = useState(false);
   const notStartedLoadState = 'not-started';
   const loadingLoadState = 'loading';
   const loadedLoadState = 'loaded';
@@ -42,7 +42,7 @@ function DeleteProfile(): React.ReactElement {
   const [resultError, setResultError] = useState<
     ApolloError | Error | undefined
   >(undefined);
-  const [deleteProfile, deleteProfileResult] = useDeleteProfile({
+  const [deleteProfile, { loading: isDeletingProfile }] = useDeleteProfile({
     onCompleted: returnedData => {
       if (returnedData) {
         trackEvent({ category: 'action', action: 'Delete profile' });
@@ -72,8 +72,11 @@ function DeleteProfile(): React.ReactElement {
     ServiceConnectionsQueryVariables
   >(SERVICE_CONNECTIONS, {
     variables: createServiceConnectionsQueryVariables(i18n.language),
+    fetchPolicy: 'no-cache',
     onCompleted: () => {
       setDataLoadState(loadedLoadState);
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      handleConfirmationModal();
     },
     onError: (error: Error) => {
       setDataLoadState(errorLoadState);
@@ -93,26 +96,28 @@ function DeleteProfile(): React.ReactElement {
     [getServiceConnections, setDataLoadState, dataLoadState, serviceConnections]
   );
 
-  const onExpandingPanelChange = useCallback(
-    isOpen => {
-      if (isOpen) {
-        loadServiceConnections();
-      }
-    },
-    [loadServiceConnections]
-  );
-
-  const handleDeleteInstructions = () => {
-    setDeleteInstructions(prevState => !prevState);
+  const toggleDeleteApproval = () => {
+    setDeleteApproved(prevState => !prevState);
   };
 
   const handleConfirmationModal = () => {
-    setDeleteConfirmationModal(prevState => !prevState);
+    setShowConfirmationModal(prevState => !prevState);
     setFocusToRemoveButton();
   };
 
+  const handleDeleteClick = () => {
+    if (dataLoadState === loadingLoadState) {
+      return;
+    }
+    if (dataLoadState === loadedLoadState) {
+      handleConfirmationModal();
+    } else {
+      loadServiceConnections();
+    }
+  };
+
   const handleProfileDelete = async () => {
-    setDeleteConfirmationModal(false);
+    setShowConfirmationModal(false);
 
     if (serviceConnections === undefined) {
       throw Error('Could not find services to delete');
@@ -120,9 +125,8 @@ function DeleteProfile(): React.ReactElement {
 
     deleteProfile();
   };
-  const initiallyOpen = deleteProfileResult.loading;
 
-  const ServiceConnectionLoadIndicator = () => (
+  const LoadIndicator = ({ text }: { text: string }) => (
     <div
       className={styles['loading-info']}
       aria-live="polite"
@@ -130,7 +134,7 @@ function DeleteProfile(): React.ReactElement {
       data-testid="delete-profile-load-indicator"
     >
       <LoadingSpinner small />
-      <p>{t('deleteProfile.loadingServices')}</p>
+      <p>{text}</p>
     </div>
   );
   const ServiceConnectionLoadError = () => (
@@ -149,46 +153,47 @@ function DeleteProfile(): React.ReactElement {
     dataLoadState === errorLoadState ? (
       <ServiceConnectionLoadError />
     ) : (
-      <ServiceConnectionLoadIndicator />
+      <LoadIndicator text={t('deleteProfile.loadingServices')} />
     );
 
+  if (isDeletingProfile) {
+    return (
+      <div data-testid={'deleting-profile'}>
+        <h2>{t('deleteProfile.title')}</h2>
+        <LoadIndicator text={`${t('notification.removing')}...`} />
+      </div>
+    );
+  }
   return (
-    <React.Fragment>
-      <ExpandingPanel
-        title={t('deleteProfile.title')}
-        initiallyOpen={initiallyOpen}
-        scrollIntoViewOnMount={initiallyOpen}
-        onChange={onExpandingPanelChange}
-        dataTestId={'delete-profile'}
-      >
-        <p>{t('deleteProfile.explanation')}</p>
-        {dataLoadState !== loadedLoadState ? (
-          <LoadStateIndicator />
-        ) : (
-          <React.Fragment>
-            <Checkbox
-              onChange={handleDeleteInstructions}
-              id="deleteInstructions"
-              name="deleteInstructions"
-              checked={deleteInstructions}
-              label={t('deleteProfile.accept')}
-            />
+    <ProfileSection hasVerifiedUserData data-test-id={'delete-profile'}>
+      <h2>{t('deleteProfile.title')}</h2>
+      <p dangerouslySetInnerHTML={{ __html: t('deleteProfile.explanation') }} />
 
-            <Button
-              type="button"
-              onClick={handleConfirmationModal}
-              disabled={!deleteInstructions}
-              className={styles.button}
-              id={removeButtonId}
-            >
-              {t('deleteProfile.delete')}
-            </Button>
-          </React.Fragment>
-        )}
-      </ExpandingPanel>
+      {dataLoadState == loadingLoadState || dataLoadState == errorLoadState ? (
+        <LoadStateIndicator />
+      ) : (
+        <div className={styles['checkbox-container']}>
+          <Checkbox
+            onChange={toggleDeleteApproval}
+            id="deleteInstructions"
+            name="deleteInstructions"
+            checked={isDeleteApproved}
+            label={t('deleteProfile.accept')}
+          />
 
+          <Button
+            type="button"
+            onClick={handleDeleteClick}
+            disabled={!isDeleteApproved}
+            className={styles.button}
+            id={removeButtonId}
+          >
+            {t('deleteProfile.delete')}
+          </Button>
+        </div>
+      )}
       <ConfirmationModal
-        isOpen={deleteConfirmationModal}
+        isOpen={showConfirmationModal}
         onClose={handleConfirmationModal}
         onConfirm={handleProfileDelete}
         content={() => <ModalServicesContent data={serviceConnections} />}
@@ -199,7 +204,7 @@ function DeleteProfile(): React.ReactElement {
         error={resultError}
         onClose={() => setResultError(undefined)}
       />
-    </React.Fragment>
+    </ProfileSection>
   );
 }
 
