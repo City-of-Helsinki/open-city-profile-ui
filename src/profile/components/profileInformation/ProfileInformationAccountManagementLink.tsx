5cf7465c33ced971f70fd393b32f246b2e0c457e
Niko Helle <niko.hele@hyperactive.fi>
2022-08-11 15:16:20 +0300

    (HP-1468) Remove link to the external account used in login.
    Removed elements, styles and translations.

--- src/profile/components/profileInformation/ProfileInformationAccountManagementLink.tsx
+++ src/profile/components/profileInformation/ProfileInformationAccountManagementLink.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { IconAngleRight, IconProps } from 'hds-react';
 import { useTranslation } from 'react-i18next';
 
 import useProfile from '../../../auth/useProfile';
 import LabeledValue from '../../../common/labeledValue/LabeledValue';
-import {
-  getAmrStatic,
-  getAmrUrl,
-} from './profileInformationAccountManagementLinkUtils';
+import { getAmrStatic } from './profileInformationAccountManagementLinkUtils';
 import styles from './profileInformationAccountManagementLink.module.css';
-import { Link } from '../../../common/copyOfHDSLink/Link';
 
 function ProfileInformationAccountManagementLink(): React.ReactElement | null {
   const { t } = useTranslation();
@@ -21,12 +16,8 @@ function ProfileInformationAccountManagementLink(): React.ReactElement | null {
     return null;
   }
 
-  const authenticationMethodReferenceUrl = getAmrUrl(amr);
   const authenticationMethodReferenceName = t(`identityProvider.${amr}`);
 
-  const LinkIcon = (props: IconProps) => (
-    <IconAngleRight {...props} size={'s'} />
-  );
   return (
     <div className={styles['container']}>
       <div className={styles['label-section']}>
@@ -34,16 +25,6 @@ function ProfileInformationAccountManagementLink(): React.ReactElement | null {
           label={t('profileInformation.authenticationMethod')}
           value={authenticationMethodReferenceName}
         />
-      </div>
-      <div className={styles['link']}>
-        <Link
-          href={authenticationMethodReferenceUrl}
-          iconReplacement={LinkIcon}
-          external
-          openInNewTab
-        >
-          {t('profileInformation.doGoToAccountManagement')}
-        </Link>
       </div>
     </div>
   );
