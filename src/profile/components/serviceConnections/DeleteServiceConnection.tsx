81f88b12fcb984a4df9fc0776128fd0af487d3a1
Niko Helle <niko.hele@hyperactive.fi>
2022-09-23 12:03:14 +0300

    (hp-1475) Added DeleteServiceConnection component
    Visully adds a "Remove"-button to each ServiceConnection.
    
    Also responsible of showing the confirmation modal and uses useDeleteServiceConnection for handling data queries.

---
+++ src/profile/components/serviceConnections/DeleteServiceConnection.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import * as Sentry from '@sentry/browser';
+import { Button, LoadingSpinner, Notification } from 'hds-react';
+
+import ConfirmationModal from '../modals/confirmationModal/ConfirmationModal';
+import useDeleteServiceConnection from '../../../gdprApi/useDeleteServiceConnection';
+import { ServiceConnectionData } from '../../helpers/getServiceConnectionData';
+
+function DeleteServiceConnection(props: {
+  service: ServiceConnectionData;
+  onDeletion: (deletedService: ServiceConnectionData) => void;
+}): React.ReactElement | null {
+  const { service } = props;
+  const { t } = useTranslation();
+  const [resultError, setResultError] = useState(false);
+  const [userRequestedDeletion, setUserRequestedDeletion] = useState(false);
+
+  const [
+    deleteConnection,
+    queryStatus,
+    accessCodeStatus,
+  ] = useDeleteServiceConnection(service.name, {
+    fetchPolicy: 'no-cache',
+    onError: errorEvent => {
+      if (errorEvent.graphQLErrors) {
+        errorEvent.graphQLErrors.forEach(graphQlError => {
+          Sentry.captureException(new Error(graphQlError.message));
+        });
+      } else {
+        Sentry.captureException(errorEvent);
+      }
+      setResultError(true);
+    },
+    onCompleted: () => {
+      props.onDeletion(service);
+    },
+  });
+
+  if (queryStatus.data) {
+    return null;
+  }
+  if (accessCodeStatus.loading || queryStatus.loading) {
+    return <LoadingSpinner small />;
+  }
+  console.log('accessCodeStatus', accessCodeStatus);
+  console.log('queryStatus', queryStatus);
+  if (resultError) {
+    return (
+      <Notification
+        type={'error'}
+        label={t('notification.genericError')}
+        dataTestId={'service-connections-load-error'}
+      >
+        <p>{t('notification.defaultErrorText')}</p>
+        <Button
+          onClick={() => {
+            deleteConnection();
+          }}
+        >
+          {t('notification.tryAgain')}
+        </Button>
+      </Notification>
+    );
+  }
+
+  const onClick = () => {
+    setUserRequestedDeletion(true);
+    deleteConnection();
+  };
+  const onCloseConfirmationModal = () => {
+    setUserRequestedDeletion(false);
+  };
+  const onConfirmConfirmationModal = () => deleteConnection();
+  const ModalContent = () => <p>voidaan poistaa</p>;
+  return (
+    <>
+      <button onClick={onClick}>Remove</button>
+      <ConfirmationModal
+        isOpen={userRequestedDeletion}
+        onClose={onCloseConfirmationModal}
+        onConfirm={onConfirmConfirmationModal}
+        content={() => <ModalContent />}
+        title={t('deleteProfileModal.title')}
+        actionButtonText={t('deleteProfileModal.delete')}
+      />
+    </>
+  );
+}
+
+export default DeleteServiceConnection;
