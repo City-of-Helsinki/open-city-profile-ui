9426d00468aa2ebd7b4b94fe2b3181ad94e2a371
Niko Helle <niko.hele@hyperactive.fi>
2022-10-18 11:26:02 +0300

    (hp-1475) Use new hooks in DeleteProfile.
    All loads can be done outside the component, in hooks.

--- src/profile/components/deleteProfile/DeleteProfile.tsx
+++ src/profile/components/deleteProfile/DeleteProfile.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import { ApolloError, useLazyQuery } from '@apollo/client';
-import { loader } from 'graphql.macro';
+import React, { useState } from 'react';
+import { ApolloError } from '@apollo/client';
 import { useTranslation } from 'react-i18next';
 import * as Sentry from '@sentry/browser';
 import { Button, LoadingSpinner, Notification } from 'hds-react';
@@ -8,119 +7,119 @@ import { useHistory } from 'react-router';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
 
 import ConfirmationModal from '../modals/confirmationModal/ConfirmationModal';
-import {
-  ServiceConnectionsQueryVariables,
-  ServiceConnectionsRoot,
-} from '../../../graphql/typings';
 import styles from './deleteProfile.module.css';
 import useDeleteProfile from '../../../gdprApi/useDeleteProfile';
 import ModalServicesContent from '../modals/deleteProfileContent/DeleteProfileContent';
 import { useFocusSetter } from '../../hooks/useFocusSetter';
 import DeleteProfileError from '../modals/deleteProfileError/DeleteProfileError';
-import createServiceConnectionsQueryVariables from '../../helpers/createServiceConnectionsQueryVariables';
 import ProfileSection from '../../../common/profileSection/ProfileSection';
-import { useScrollIntoView } from '../../hooks/useScrollIntoView';
-
-const SERVICE_CONNECTIONS = loader(
-  '../../graphql/ServiceConnectionsQuery.graphql'
-);
 
 function DeleteProfile(): React.ReactElement {
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
   const notStartedLoadState = 'not-started';
   const loadingLoadState = 'loading';
   const loadedLoadState = 'loaded';
+  const dryRunComplete = 'dry-run-complete';
   const errorLoadState = 'error';
-  const [dataLoadState, setDataLoadState] = useState<
-    | typeof notStartedLoadState
-    | typeof loadingLoadState
-    | typeof loadedLoadState
-    | typeof errorLoadState
-  >(notStartedLoadState);
   const history = useHistory();
   const { trackEvent } = useMatomo();
   const [resultError, setResultError] = useState<
     ApolloError | Error | undefined
   >(undefined);
-  const [deleteProfile, { loading: isDeletingProfile }] = useDeleteProfile({
-    onCompleted: returnedData => {
-      if (returnedData) {
-        trackEvent({ category: 'action', action: 'Delete profile' });
-        history.push('/profile-deleted');
-      }
-    },
-    onError: error => {
-      if (error.graphQLErrors) {
-        error.graphQLErrors.forEach(graphQlError => {
-          Sentry.captureException(new Error(graphQlError.message));
-        });
-      } else {
-        Sentry.captureException(error);
-      }
-      setResultError(error);
-    },
-  });
-
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const [removeButtonId, setFocusToRemoveButton] = useFocusSetter({
     targetId: `delete-profile-button`,
   });
 
-  const [scrollIntoViewRef] = useScrollIntoView(isDeletingProfile);
-
-  const [getServiceConnections, { data: serviceConnections }] = useLazyQuery<
-    ServiceConnectionsRoot,
-    ServiceConnectionsQueryVariables
-  >(SERVICE_CONNECTIONS, {
-    variables: createServiceConnectionsQueryVariables(i18n.language),
-    fetchPolicy: 'no-cache',
-    onCompleted: () => {
-      setDataLoadState(loadedLoadState);
-      // eslint-disable-next-line @typescript-eslint/no-use-before-define
-      handleConfirmationModal();
-    },
-    onError: (error: Error) => {
-      setDataLoadState(errorLoadState);
-      Sentry.captureException(error);
-    },
-  });
-
-  const loadServiceConnections = useCallback(
-    (reloadAfterError = false) => {
-      if (dataLoadState !== loadedLoadState && serviceConnections) {
-        setDataLoadState(loadedLoadState);
-      } else if (dataLoadState === notStartedLoadState || reloadAfterError) {
-        getServiceConnections();
-        setDataLoadState(loadingLoadState);
-      }
-    },
-    [getServiceConnections, setDataLoadState, dataLoadState, serviceConnections]
-  );
-
   const handleConfirmationModal = () => {
     setShowConfirmationModal(prevState => !prevState);
     setFocusToRemoveButton();
   };
+  const [
+    dryRunDeletion,
+    deleteProfile,
+    {
+      loading,
+      dryRun,
+      serviceConnections,
+      dryRunError,
+      deleteError,
+      dryRunFailures,
+      dryRunResult,
+      deleteResult,
+    },
+  ] = useDeleteProfile({
+    onDeleteCompleted: () => {
+      trackEvent({ category: 'action', action: 'Delete profile' });
+      history.push('/profile-deleted');
+    },
+    onDryRunCompleted: (result, failures) => {
+      handleConfirmationModal();
+    },
+    onError: error => {
+      if (dryRun) {
+        handleConfirmationModal();
+      } else {
+        if ((error as ApolloError).graphQLErrors) {
+          (error as ApolloError).graphQLErrors.forEach(graphQlError => {
+            Sentry.captureException(new Error(graphQlError.message));
+          });
+        } else {
+          Sentry.captureException(error);
+        }
+        setResultError(error as Error);
+      }
+    },
+  });
+
+  const getLoadState = () => {
+    /*console.log('dataLoadState', {
+      loading,
+      dryRun,
+      serviceConnections,
+      dryRunError,
+      deleteError,
+      dryRunFailures,
+      dryRunResult,
+      deleteResult,
+    });*/
+    if (loading) {
+      return loadingLoadState;
+    }
+    if (loading) {
+      return loadingLoadState;
+    }
+    if (dryRunError || deleteError) {
+      return errorLoadState;
+    }
+    if (dryRun && (dryRunFailures || dryRunResult)) {
+      return dryRunComplete;
+    }
+    if (deleteResult) {
+      return loadedLoadState;
+    }
+    return notStartedLoadState;
+  };
+
+  const dataLoadState = getLoadState();
+
+  console.log('dataLoadState', dataLoadState);
 
   const handleDeleteClick = () => {
+    console.log('handleDeleteClick', dataLoadState);
     if (dataLoadState === loadingLoadState) {
       return;
     }
     if (dataLoadState === loadedLoadState) {
-      handleConfirmationModal();
+      dryRunDeletion();
     } else {
-      loadServiceConnections();
+      dryRunDeletion();
     }
   };
 
   const handleProfileDelete = async () => {
     setShowConfirmationModal(false);
-
-    if (serviceConnections === undefined) {
-      throw Error('Could not find services to delete');
-    }
-
     deleteProfile();
   };
 
@@ -139,7 +138,7 @@ function DeleteProfile(): React.ReactElement {
     <Notification label={t('notification.defaultErrorText')} type={'error'}>
       <Button
         type="button"
-        onClick={() => loadServiceConnections(true)}
+        onClick={() => dryRunDeletion()}
         className={styles.button}
         data-testid="reload-service-connections"
       >
@@ -154,19 +153,10 @@ function DeleteProfile(): React.ReactElement {
       <LoadIndicator text={t('deleteProfile.loadingServices')} />
     );
 
-  if (isDeletingProfile) {
-    return (
-      <div data-testid={'deleting-profile'}>
-        <h2 ref={scrollIntoViewRef}>{t('deleteProfile.title')}</h2>
-        <LoadIndicator text={`${t('notification.removing')}...`} />
-      </div>
-    );
-  }
   return (
     <ProfileSection hasVerifiedUserData data-test-id={'delete-profile'}>
       <h2>{t('deleteProfile.title')}</h2>
       <p dangerouslySetInnerHTML={{ __html: t('deleteProfile.explanation') }} />
-
       {dataLoadState === loadingLoadState ||
       dataLoadState === errorLoadState ? (
         <LoadStateIndicator />
@@ -184,7 +174,12 @@ function DeleteProfile(): React.ReactElement {
         isOpen={showConfirmationModal}
         onClose={handleConfirmationModal}
         onConfirm={handleProfileDelete}
-        content={() => <ModalServicesContent data={serviceConnections} />}
+        content={() => (
+          <ModalServicesContent
+            data={serviceConnections}
+            failedDryRunConnections={dryRunFailures}
+          />
+        )}
         title={t('deleteProfileModal.title')}
         actionButtonText={t('deleteProfileModal.delete')}
       />
