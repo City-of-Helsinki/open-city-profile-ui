5670556336f8292b45058e34260dfe8c9d9f330b
Niko Helle <niko.hele@hyperactive.fi>
2022-10-18 11:24:58 +0300

    (hp-1475) Dry runs in useDeleteProfile

--- src/gdprApi/useDeleteProfile.ts
+++ src/gdprApi/useDeleteProfile.ts
@@ -1,9 +1,5 @@
-import React from 'react';
-import {
-  useMutation,
-  MutationHookOptions,
-  MutationResult,
-} from '@apollo/client';
+import React, { useRef } from 'react';
+import { useMutation, ApolloError } from '@apollo/client';
 import { loader } from 'graphql.macro';
 
 import {
@@ -11,19 +7,59 @@ import {
   GdprDeleteMyProfileMutationVariables,
 } from '../graphql/generatedTypes';
 import useServiceConnectionsAuthorizationCode from './useServiceConnectionsAuthorizationCode';
+import { ServiceConnectionData } from '../profile/helpers/getServiceConnectionData';
 
 const DELETE_PROFILE = loader('./graphql/GdprDeleteMyProfileMutation.graphql');
 
-function useDeleteProfile(
-  options: MutationHookOptions<
-    GdprDeleteMyProfileMutation,
-    GdprDeleteMyProfileMutationVariables
-  >
-): [() => void, MutationResult<GdprDeleteMyProfileMutation>] {
+type Result = {
+  loading: boolean;
+  dryRun: boolean;
+  dryRunResult?: GdprDeleteMyProfileMutation;
+  dryRunFailures?: string[];
+  dryRunError?: ApolloError['networkError'];
+  deleteError?: ApolloError;
+  deleteResult?: GdprDeleteMyProfileMutation;
+  serviceConnections?: ServiceConnectionData[];
+};
+
+type Options = {
+  onDeleteCompleted: (result?: GdprDeleteMyProfileMutation) => void;
+  onDryRunCompleted: (
+    result?: GdprDeleteMyProfileMutation,
+    failures?: Result['dryRunFailures']
+  ) => void;
+  onError: (
+    e: ApolloError | ApolloError['networkError'],
+    wasDryRun: boolean
+  ) => void;
+};
+
+function useDeleteProfile(options: Options): [() => void, () => void, Result] {
+  const isDryRunRef = useRef<boolean>(false);
   const [deleteProfile, queryResult] = useMutation<
     GdprDeleteMyProfileMutation,
     GdprDeleteMyProfileMutationVariables
-  >(DELETE_PROFILE, options);
+  >(DELETE_PROFILE, {
+    fetchPolicy: 'no-cache',
+    onCompleted: data => {
+      console.log('onCompleted', data);
+      if (isDryRunRef.current) {
+        options.onDryRunCompleted(
+          queryResult.data || undefined,
+          queryResult.error
+            ? queryResult.error.graphQLErrors.map(error => error.message)
+            : undefined
+        );
+      } else if (options?.onDeleteCompleted) {
+        options.onDeleteCompleted(data);
+      }
+    },
+    onError: e => {
+      if (options?.onError) {
+        options.onError(e, isDryRunRef.current);
+      }
+    },
+  });
 
   const handleAuthorizationCodeCallback = React.useCallback(
     (authorizationCode: string | null) => {
@@ -31,9 +67,9 @@ function useDeleteProfile(
         const variablesWithAuthorizationCode = {
           input: {
             authorizationCode,
+            dryRun: isDryRunRef.current,
           },
         };
-
         deleteProfile({
           variables: variablesWithAuthorizationCode,
         });
@@ -49,15 +85,59 @@ function useDeleteProfile(
     onCompleted: e => {
       handleAuthorizationCodeCallback(e);
     },
-    onError: options?.onError,
+    onError: e => {
+      if (options?.onError) {
+        options.onError(e, isDryRunRef.current);
+      }
+    },
   });
 
-  const injectedMutationResult = {
-    ...queryResult,
-    loading: authorizationCodeStatus.loading || queryResult.loading,
+  const loading = authorizationCodeStatus.loading || queryResult.loading;
+  const waitingForMutationStart =
+    authorizationCodeStatus.complete && !queryResult.data && !queryResult.error;
+  const dryRunResult =
+    isDryRunRef.current && !loading
+      ? {
+          error: queryResult.error,
+          result: queryResult.data,
+        }
+      : {};
+
+  const deleteResult =
+    !isDryRunRef.current && !loading
+      ? {
+          error: queryResult.error,
+          result: queryResult.data,
+        }
+      : {};
+
+  const result: Result = {
+    dryRun: isDryRunRef.current,
+    dryRunFailures: dryRunResult.error
+      ? dryRunResult.error.graphQLErrors.map(error => error.message)
+      : undefined,
+    dryRunError: dryRunResult.error
+      ? dryRunResult.error.networkError
+      : undefined,
+    dryRunResult: dryRunResult.result || undefined,
+    deleteError: deleteResult.error || undefined,
+    deleteResult: deleteResult.result || undefined,
+    loading: loading || waitingForMutationStart,
+    serviceConnections: authorizationCodeStatus.serviceConnections,
   };
 
-  return [getAuthorizationCode, injectedMutationResult];
+  return [
+    () => {
+      console.log('dry run');
+      isDryRunRef.current = true;
+      getAuthorizationCode();
+    },
+    () => {
+      isDryRunRef.current = false;
+      getAuthorizationCode();
+    },
+    result,
+  ];
 }
 
 export default useDeleteProfile;
