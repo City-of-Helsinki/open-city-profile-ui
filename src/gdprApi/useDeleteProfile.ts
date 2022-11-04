3f118414b49dbfdcc5500da9ac2a468bd8410f4b
Niko Helle <niko.hele@hyperactive.fi>
2022-11-04 11:23:02 +0200

    (HP-1657) Use useServiceConnectionsAuthorizationCode in profile delete

--- src/gdprApi/useDeleteProfile.ts
+++ src/gdprApi/useDeleteProfile.ts
@@ -3,7 +3,6 @@ import {
   useMutation,
   MutationHookOptions,
   MutationResult,
-  useLazyQuery,
 } from '@apollo/client';
 import { loader } from 'graphql.macro';
 
@@ -11,14 +10,9 @@ import {
   GdprDeleteMyProfileMutation,
   GdprDeleteMyProfileMutationVariables,
 } from '../graphql/generatedTypes';
-import { GdprServiceConnectionsRoot } from '../graphql/typings';
-import { getDeleteScopes } from './utils';
-import useAuthorizationCode from './useAuthorizationCode';
+import useServiceConnectionsAuthorizationCode from './useServiceConnectionsAuthorizationCode';
 
 const DELETE_PROFILE = loader('./graphql/GdprDeleteMyProfileMutation.graphql');
-const SERVICE_CONNECTIONS = loader(
-  './graphql/GdprServiceConnectionsQuery.graphql'
-);
 
 function useDeleteProfile(
   options: MutationHookOptions<
@@ -39,7 +33,6 @@ function useDeleteProfile(
             authorizationCode,
           },
         };
-
         deleteProfile({
           variables: variablesWithAuthorizationCode,
         });
@@ -48,41 +41,24 @@ function useDeleteProfile(
     [deleteProfile]
   );
 
-  const [startFetchingAuthorizationCode, isAuthorizing] = useAuthorizationCode(
-    'useDeleteProfile',
-    handleAuthorizationCodeCallback
-  );
-
-  const handleDownloadActionInitialization = React.useCallback(
-    serviceConnectionsResult => {
-      const deleteScopes = getDeleteScopes(serviceConnectionsResult);
-
-      startFetchingAuthorizationCode(deleteScopes);
+  const [
+    getAuthorizationCode,
+    authorizationCodeStatus,
+  ] = useServiceConnectionsAuthorizationCode({
+    requiredGdprScope: 'delete',
+    deferredAction: 'useDeleteProfile',
+    onCompleted: e => {
+      handleAuthorizationCodeCallback(e);
     },
-    [startFetchingAuthorizationCode]
-  );
-
-  const [getServiceConnections] = useLazyQuery<GdprServiceConnectionsRoot>(
-    SERVICE_CONNECTIONS,
-    {
-      fetchPolicy: 'no-cache',
-      onCompleted: data => {
-        handleDownloadActionInitialization(data);
-      },
-      onError: e => {
-        if (options.onError) {
-          options.onError(e);
-        }
-      },
-    }
-  );
+    onError: options?.onError,
+  });
 
   const injectedMutationResult = {
     ...queryResult,
-    loading: isAuthorizing || queryResult.loading,
+    loading: authorizationCodeStatus.loading || queryResult.loading,
   };
 
-  return [getServiceConnections, injectedMutationResult];
+  return [getAuthorizationCode, injectedMutationResult];
 }
 
 export default useDeleteProfile;
