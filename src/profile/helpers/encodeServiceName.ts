f3ebca7793d1b9de5bc10e502fb2d3bf2fd28f33
Niko Helle <niko.hele@hyperactive.fi>
2022-11-08 11:51:29 +0200

    (HP-1657) Encode service name to use it in data test ids
    "Service name" is encoded to "service-name". String is lowercased and non-word chars are replaced with "-"

---
+++ src/profile/helpers/encodeServiceName.ts
@@ -0,0 +1,10 @@
+import { Service } from '../../graphql/typings';
+import { ServiceConnectionData } from './getServiceConnectionData';
+
+export default function encodeServiceName(
+  service: Service | ServiceConnectionData
+) {
+  return String(service.name)
+    .toLowerCase()
+    .replace(/[\W]/g, '-');
+}
