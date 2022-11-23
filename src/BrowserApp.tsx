d37cc2bb9092e3e77f7433a2c6dc2d7a083df4ad
Niko Helle <niko.hele@hyperactive.fi>
2022-11-23 15:39:43 +0200

    (HP-962) Added cookie consent modal
    Modal is not shown when path is the auto login path as it is only a redirect path.
    
    Modal is not shown on cookies page either. The user won't usually land there first and the content is identical with cookie page.
    
    Modal and page have their own context (content) and do not affect each other. So making changes in one, do not change the other.
    
    For that reason too they should not be shown at the same time.
    
    Re-arranged language and route providers. Language provider rarely re-renders and should therefore be outside of route provider.

--- src/BrowserApp.tsx
+++ src/BrowserApp.tsx
@@ -4,14 +4,16 @@ import { I18nextProvider } from 'react-i18next';
 
 import App from './App';
 import i18n from './i18n/i18nInit';
+import CookieConsentModal from './cookieConsents/CookieConsentModal';
 
 function BrowserApp(): React.ReactElement {
   return (
-    <BrowserRouter>
-      <I18nextProvider i18n={i18n}>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <CookieConsentModal />
         <App />
-      </I18nextProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </I18nextProvider>
   );
 }
 
