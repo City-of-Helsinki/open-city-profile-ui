4a1ed76b217e5114bd042ced3b1afc0d23eab008
Niko Helle <niko.hele@hyperactive.fi>
2023-03-01 11:32:09 +0200

    (HP-1796) Moved contents of the MultiItemEditor to actual components
    MultiItemEditor is not needed anymore as a wrapper component. It is very simple and therefore not needed as a wrapper.

--- src/profile/components/addressEditor/AddressEditor.tsx
+++ src/profile/components/addressEditor/AddressEditor.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import AccessibilityFieldHelpers from '../../../common/accessibilityFieldHelpers/AccessibilityFieldHelpers';
 import ProfileSection from '../../../common/profileSection/ProfileSection';
-import MultiItemEditor from '../multiItemEditor/MultiItemEditor';
+import { EditDataType } from '../../helpers/editData';
+import { useCommonEditHandling } from '../../hooks/useCommonEditHandling';
+import EditingNotifications from '../editingNotifications/EditingNotifications';
+import ConfirmationModal from '../modals/confirmationModal/ConfirmationModal';
+import MultiItemAddressRow from '../multiItemAddressRow/MultiItemAddressRow';
 
-const AddressEditor = (): React.ReactElement | null => (
-  <ProfileSection>
-    <MultiItemEditor dataType="addresses" />
-  </ProfileSection>
-);
+const AddressEditor = (): React.ReactElement | null => {
+  const dataType: EditDataType = 'addresses';
 
+  const editHandler = useCommonEditHandling({
+    dataType,
+    disableEditButtons: false,
+  });
+
+  const { noticationContent, confirmationModalProps } = editHandler;
+  return (
+    <ProfileSection>
+      <MultiItemAddressRow editHandler={editHandler} />
+      <AccessibilityFieldHelpers dataType={dataType} />
+      <EditingNotifications
+        content={noticationContent.content}
+        dataType={dataType}
+      />
+      <ConfirmationModal {...confirmationModalProps} />
+    </ProfileSection>
+  );
+};
 export default AddressEditor;
--- src/profile/components/phoneNumberEditor/PhoneNumberEditor.tsx
+++ src/profile/components/phoneNumberEditor/PhoneNumberEditor.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import AccessibilityFieldHelpers from '../../../common/accessibilityFieldHelpers/AccessibilityFieldHelpers';
 import commonFormStyles from '../../../common/cssHelpers/form.module.css';
-import MultiItemEditor from '../multiItemEditor/MultiItemEditor';
+import { EditDataType } from '../../helpers/editData';
+import { useCommonEditHandling } from '../../hooks/useCommonEditHandling';
+import EditingNotifications from '../editingNotifications/EditingNotifications';
+import ConfirmationModal from '../modals/confirmationModal/ConfirmationModal';
+import MultiItemPhoneRow from '../multiItemPhoneRow/MultiItemPhoneRow';
 
 const PhoneNumberEditor = (): React.ReactElement | null => {
+  const dataType: EditDataType = 'phones';
   const { t } = useTranslation();
+  const editHandler = useCommonEditHandling({
+    dataType,
+    disableEditButtons: false,
+  });
+
+  const { noticationContent, confirmationModalProps } = editHandler;
   return (
     <>
       <div className={commonFormStyles['editor-description-container']}>
         <h2>{t('profileInformation.contact')}</h2>
       </div>
-      <MultiItemEditor dataType="phones" />
+      <MultiItemPhoneRow editHandler={editHandler} />
+      <AccessibilityFieldHelpers dataType={dataType} />
+      <EditingNotifications
+        content={noticationContent.content}
+        dataType={dataType}
+      />
+      <ConfirmationModal {...confirmationModalProps} />
     </>
   );
 };
