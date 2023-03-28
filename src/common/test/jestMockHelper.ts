eef8cdc75bad48c4eb6b6697bee69474bb47e625
Niko Helle <niko.hele@hyperactive.fi>
2023-03-28 14:55:02 +0300

    (HP-1833) New helper to return mock fn call args

--- src/common/test/jestMockHelper.ts
+++ src/common/test/jestMockHelper.ts
@@ -2,3 +2,8 @@ export const getMockCallArgs = (
   mockFn: jest.Mock,
   argumentIndex = 0
 ): unknown[] => mockFn.mock.calls.map(call => call[argumentIndex]);
+
+export const getAllLastMockCallArgs = (mockFn: jest.Mock): unknown[] => {
+  const index = mockFn.mock.calls.length - 1;
+  return mockFn.mock.calls[index];
+};
