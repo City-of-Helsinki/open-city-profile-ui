eca1ff28948e1924578a4778c85d974b9e19b8d9
Niko Helle <niko.hele@hyperactive.fi>
2023-04-05 17:08:36 +0300

    (login-to) Custom error type for login clients
    Easier to debug with this

---
+++ src/auth/login-client-error.ts
@@ -0,0 +1,35 @@
+export type LoginClientErrorType =
+  | 'INVALID_OR_EXPIRED_USER'
+  | 'API_TOKENS_FAILED'
+  | 'INVALID_API_TOKENS'
+  | 'API_TOKEN_NETWORK_OR_CORS_ERROR'
+  | 'NO_API_TOKEN_URL';
+
+class LoginClientError extends Error {
+  constructor(
+    public message: string,
+    public type: LoginClientErrorType,
+    public originalError?: Error | null
+  ) {
+    super(message);
+    this.type = type;
+    this.originalError = originalError;
+  }
+  get isInvalidUserError() {
+    return this.type === 'INVALID_OR_EXPIRED_USER';
+  }
+  get isInvalidApiTokensError() {
+    return this.type === 'INVALID_API_TOKENS';
+  }
+  get isApiTokensFailedError() {
+    return this.type === 'API_TOKENS_FAILED';
+  }
+  get isMissingApiTokenUrlError() {
+    return this.type === 'NO_API_TOKEN_URL';
+  }
+  get isApiTokenNetWorkOrCorsError() {
+    return this.type === 'API_TOKEN_NETWORK_OR_CORS_ERROR';
+  }
+}
+
+export default LoginClientError;
