20a6368980696bbcd9320aae75766a00ef62fa4e
Niko Helle <niko.hele@hyperactive.fi>
2023-04-11 15:17:27 +0300

    (login-to) Added new UNAUTHORIZED_SESSION error type

--- src/auth/login-client-error.ts
+++ src/auth/login-client-error.ts
@@ -1,5 +1,6 @@
 export type LoginClientErrorType =
   | 'INVALID_OR_EXPIRED_USER'
+  | 'UNAUTHORIZED_SESSION'
   | 'API_TOKENS_FAILED'
   | 'INVALID_API_TOKENS'
   | 'API_TOKEN_NETWORK_OR_CORS_ERROR'
@@ -34,6 +35,12 @@ class LoginClientError extends Error {
   }
   get isInvalidApiTokenError() {
     return this.type === 'USER_HAS_INVALID_TOKENS';
+  }
+  get isUnauthorizedSessionError() {
+    return this.type === 'UNAUTHORIZED_SESSION';
+  }
+  get isRenewalFailedError() {
+    return this.type === 'RENEWAL_FAILED';
   }
 }
 
