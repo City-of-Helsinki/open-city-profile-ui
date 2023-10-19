3653a7db6d4c25b56bc3c2a2ff4e6672af8bd3d7
NikoHelle <niko.helle@hyperactive.fi>
2023-10-19 23:56:21 +0300

    (HP-2023) Remove dead code

--- src/gdprApi/gdprAuthCodeManager.ts
+++
@@ -1,9 +0,0 @@
-import GdprAuthorizationTokenManager from './GdprAuthorizationCodeManager';
-
-const gdprAuthCodeManager = new GdprAuthorizationTokenManager({
-  clientId: window._env_.REACT_APP_PROFILE_BE_GDPR_CLIENT_ID as string,
-  redirectUri: `${window.location.origin}/gdpr-callback`,
-  oidcAuthority: window._env_.REACT_APP_OIDC_AUTHORITY as string,
-});
-
-export default gdprAuthCodeManager;
