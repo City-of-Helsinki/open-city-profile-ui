e366c310f78871f991a03349b041093404c9239b
Mikko Riippi <mikko.riippi@gofore.com>
2024-02-22 13:50:54 +0200

    feat: update front page and helsinki logo

--- src/common/helsinkiLogo/__tests__/HelsinkiLogo.test.tsx
+++ src/common/helsinkiLogo/__tests__/HelsinkiLogo.test.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, act } from '@testing-library/react';
 
 import i18n from '../../test/testi18nInit';
 import HelsinkiLogo from '../HelsinkiLogo';
 
-type Props = {
-  isLinkToFrontPage?: boolean;
-};
+it('language switch changes helsinki logo', async () => {
+  const { getByTestId } = render(<HelsinkiLogo />);
+  const fiSvg = getByTestId('helsinki-logo').getAttribute('src');
 
-const getWrapper = (props?: Props) => shallow(<HelsinkiLogo {...props} />);
-
-describe('renders correct logo based on language', () => {
-  it('finnish logo', () => {
-    const wrapper = getWrapper();
-    expect(wrapper.find('.logo-fi').length).toEqual(1);
-  });
-
-  it('swedish logo', () => {
+  act(() => {
+    // Change the language to swedish
     i18n.changeLanguage('sv');
-    const wrapper = getWrapper();
-    expect(wrapper.find('.logo-sv').length).toEqual(1);
   });
-});
 
-it('renders link', () => {
-  const wrapper = getWrapper({ isLinkToFrontPage: true });
-  expect(wrapper.props().to).toEqual('/');
-});
-
-it('renders span', () => {
-  const wrapper = getWrapper();
-  expect(wrapper.props().to).toBeFalsy();
+  // Check that the logo image source has changed (Helsinki to Helsingfors)
+  expect(getByTestId('helsinki-logo').getAttribute('src')).not.toBe(fiSvg);
 });
