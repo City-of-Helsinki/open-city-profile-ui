d414ece8b5750b6aaa032d12496c5bf933e81308
mikkojamG <mikko.jamia@gofore.com>
2024-07-31 13:17:44 +0300

    feat: gdpr actions report to sentry changes HP-2488

--- src/gdprApi/actions/getDownloadData.ts
+++ src/gdprApi/actions/getDownloadData.ts
@@ -65,7 +65,12 @@ const getDownloadDataExecutor: ActionExecutor = async (
     })
   );
   if (error) {
-    reportErrorsToSentry(error);
+    if (
+      !parseGraphQLError(error).isInsufficientLoaError ||
+      !parseGraphQLError(error).isAllowedError
+    ) {
+      reportErrorsToSentry(error);
+    }
 
     if (parseGraphQLError(error).isInsufficientLoaError) {
       return Promise.reject(resultTypes.insufficientLoa);
